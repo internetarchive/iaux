ffa94410633bec24c7d76be229c92d9492334623
Isa Herico-Velasco <isaherico@gmail.com>
2019-04-17 12:33:05 -0700

    Music Player, content area: show audio icon if no background photo

--- packages/ia-components/sandbox/theatres/components/audio-player/audio-player.test.js
+++ packages/ia-components/sandbox/theatres/components/audio-player/audio-player.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
 
-import { TheatreAudioPlayer } from '../../../../index';
+import TheatreAudioPlayer from './audio-player-main';
 
 
 /**
@@ -20,7 +20,6 @@ describe('TheatreAudioPlayer ', () => {
   );
   const tree = component.toJSON();
   const testInstance = component.root;
-
   test('has main sections: content window, tabs', () => {
     expect(tree).toMatchSnapshot();
     expect(tree.children.length).toEqual(2);
@@ -30,5 +29,14 @@ describe('TheatreAudioPlayer ', () => {
   test('shows iframe when referencing third party', () => {
     const iframe = testInstance.findAllByType('iframe');
     expect(iframe.length).toEqual(1);
+  });
+  test('shows media icon when there is no background photo', () => {
+    const { props } = testInstance;
+    const { backgroundPhoto } = props;
+
+    expect(backgroundPhoto).toBeFalsy();
+    const contentWindow = tree.children[0];
+    const backgroundPhotoPlaceholder = contentWindow.children[0];
+    expect(backgroundPhotoPlaceholder.props.className).toContain('no-photo');
   });
 });
