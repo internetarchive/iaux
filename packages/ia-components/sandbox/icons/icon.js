681855a5f96889d4bc3e2c18e69a765626c441a0
Bate Martin <lordbatemartin@ymail.com>
2019-04-22 21:03:32 +0100

    fixed ia improper rendering

--- packages/ia-components/sandbox/icons/icon.js
+++ packages/ia-components/sandbox/icons/icon.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Archive from './customsvg/ia-logo-white-icon';
 import iconfile from './iconfiles';
 
@@ -6,7 +6,13 @@ function defaultColor(color, iconName) {
 	const icon = iconfile.icons.find(icon => icon.tags[0] === iconName || icon.tags[1] === iconName);
 
 	if (!color) {
-		return icon.defaultColor
+		if (!icon) {
+			console.error('Couldnt locate the requested icon');
+
+		} else {
+			return icon.defaultColor
+		}
+
 	} else {
 		return color;
 	}
@@ -19,8 +25,7 @@ function getPath(iconName) {
 	if (icon) {
 		return icon.paths
 	} else {
-		console.log(`icon ${iconName} does not exist.`);
-		return '';
+		return <Archive />
 	}
 
 }
@@ -31,21 +36,25 @@ function getPath(iconName) {
  * @params See proptypes below
  */
 const Icon = props => {
-	if (props.type !== 'avatar') {
+	if (props.name === 'archive') {
 		return (
-			<svg width={props.size} height={iconfile.icons} viewBox="0 0 1024 1024" fill={defaultColor(props.color, props.name)}
-				xmlns="http://www.w3.org/2000/svg"
-				aria-labelledby={props.name}
-				role="img"
-			>
-				<title id="">{props.name}</title>
-				<path d={getPath(props.name)}></path>
-			</svg>
+			<Archive />
 		)
 	} else {
-		//TODO return <Avatar url  size/>
+		return (<svg width={props.size} height={props.size} viewBox="0 0 1024 1024" fill={defaultColor(props.color, props.name)}
+			xmlns="http://www.w3.org/2000/svg"
+			aria-labelledby={props.name}
+			role="img"
+		>
+			<title id="">{props.name}</title>
+			<path d={getPath(props.name)}></path>
+		</svg>
+		)
+
 	}
 };
-
+Icon.defaultProps = {
+	size: 24
+};
 
 export default Icon
