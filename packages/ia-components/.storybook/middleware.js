78ab8ca05bff47762358c358118165f4927bc505
Isa Herico-Velasco <isaherico@gmail.com>
2019-04-23 10:11:45 -0700

    Updating localhost proxy to programmatically create routes

--- packages/ia-components/.storybook/middleware.js
+++ packages/ia-components/.storybook/middleware.js
@@ -1,37 +1,30 @@
 const proxy = require('http-proxy-middleware');
 
-module.exports = function expressMiddleware (router) {
+const base_url = "https://archive.org";
 
-  router.use('/download', proxy({
-    target: 'https://archive.org/download',
+const constructOptions = (route) => {
+  const target = `${base_url}/${route}`;
+  const pathRewrite = {};
+  pathRewrite[`^/${route}`] = target;
+  return {
+    target,
     logLevel: 'debug',
     changeOrigin: true,
-    pathRewrite: {
-      '^/download': 'https://archive.org/download'
-    }
-  }));
-  router.use('/images', proxy({
-    target: 'https://archive.org/images',
-    logLevel: 'debug',
-    changeOrigin: true,
-    pathRewrite: {
-      '^/images': 'https://archive.org/images'
-    }
-  }));
-  router.use('/serve', proxy({
-    target: 'https://archive.org/serve',
-    logLevel: 'debug',
-    changeOrigin: true,
-    pathRewrite: {
-      '^/serve': 'https://archive.org/serve'
-    }
-  }));
+    pathRewrite,
+  }
+};
+
+module.exports = function expressMiddleware (router) {
+  ['details', 'download', 'embed', 'images', 'metadata', 'serve'].forEach((route) => {
+    router.use(`/${route}`, proxy(constructOptions(route)))
+  });
+
   router.use('/api', proxy({
-    target: 'https://archive.org',
+    target: base_url,
     logLevel: 'debug',
     changeOrigin: true,
     pathRewrite: {
-      '^/api': 'https://archive.org'
+      '^/api': base_url
     }
   }));
 };
