e43492bb55b02ee5d81f1ea082744ffaf8d9eb82
Mitra Ardron <mitra@mitra.biz>
2019-05-03 14:10:17 +1000

    DetailsActionButtons - the <div class=action-buttons> block of Details page

---
+++ packages/ia-components/sandbox/details/DetailsActionButtons.js
@@ -0,0 +1,39 @@
+//const debug = require('debug')('dweb-archive:DetailsActionButtons');
+import React from 'react';
+import IAReactComponent from '../IAReactComponent'; // Encapsulates differences between dweb-archive/ReactFake and iaux/React
+import DetailsFlags from './DetailsFlags';
+import {AnchorModalGo, ButtonModalGo} from './ModalGo';
+
+/* DetailsActionButtons are a group of buttons, usually shown on the right, that include bookmarking, sharing and flagging.
+    Its currently used in dweb-archive/Details.js
+ */
+
+export default class DetailsActionButtons extends IAReactComponent {
+
+    constructor(props)
+    {
+        super(props) //identifier, title
+    }
+    render() {
+        const bookmarksAddURL = `https://archive.org/bookmarks.php?add_bookmark=1&amp;mediatype=image&amp;identifier=${this.props.identifier}&amp;title=${this.props.title}`; //TODO find way to submit distributed
+        return (
+            <div className="action-buttons">
+                <div className="topinblock">
+                    <AnchorModalGo className="button " opts={{favorite:1}}  href={bookmarksAddURL} id="favorite-button" aria-haspopup="true"
+                                   data-target="#confirm-modal" data-toggle="tooltip"
+                                   data-container="body" data-placement="bottom" title="Favorite this item">
+                        <span className="iconochive-favorite" aria-hidden="true"></span><span className="sr-only">favorite</span>
+                    </AnchorModalGo>
+                </div>
+                <div className="topinblock">
+                    <ButtonModalGo id="share-button" className="button" opts={{ignore_lnk:1,shown:AJS.embed_codes_adjust}}
+                                   type="button" aria-haspopup="true"
+                                   data-target="#cher-modal" data-toggle="tooltip" data-container="body" data-placement="bottom"
+                                   title="Share this item">
+                        <span className="iconochive-share" aria-hidden="true"></span><span className="sr-only">share</span></ButtonModalGo>
+                </div>
+                <DetailsFlags/>
+            </div>
+        )
+    }
+}
