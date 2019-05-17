70b43d4f7f988d57bfd6c8ae6ed6ba2a6950f62c
Mitra Ardron <mitra@mitra.biz>
2019-05-17 20:15:25 +1000

    Fix issue with new canonicalUrl and remove now unused DetailsFlags.jsx

--- packages/ia-components/sandbox/details/DetailsFlags.jsx
+++
@@ -1,8 +0,0 @@
-//const debug = require('debug')('dweb-archive:DetailsFlags');
-import React from 'react';
-import IAReactComponent from '../IAReactComponent'; // Encapsulates differences between dweb-archive/ReactFake and iaux/React
-//import PropTypes from 'prop-types' // Not currently used by IAUX
-
-/* DetailsFlags is a component intended for the details page to show any content flagging.
-    Its currently used in dweb-archive/Details.js
- */
