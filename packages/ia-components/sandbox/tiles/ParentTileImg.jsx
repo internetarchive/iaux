7accfd701428a66185a4985a80942edd19d093d6
Mitra Ardron <mitra@mitra.biz>
2019-06-12 16:16:40 +1000

    Just documentation and code cleanup/linting

--- packages/ia-components/sandbox/tiles/ParentTileImg.jsx
+++ packages/ia-components/sandbox/tiles/ParentTileImg.jsx
@@ -4,15 +4,21 @@ import React from 'react';
 import IAReactComponent from '../IAReactComponent';
 
 export default class ParentTileImg extends IAReactComponent {
-  /* Unused...
-    static propTypes = {
-        identifier: PropTypes.string, OR
-        member: PropTypes.object (will be followed to get parentidentifier) (the object we want the parent of)
-        parentidentifier: PropTypes.string,
-    };
-    */
+  /* <ParentTileImg
+   *  parentidentifier: string: identifier of the parent item want to display image for (optional)
+   *  member: ArchiveMember or {collection0thumbnaillinks} or has collection0() method returning identifier of parent (optional)
+   * />
+   *
+   * Behavior:
+   *  Renders on Dweb:  as <span> in which an <img> is built dynamically TODO switch to re-rendering to rebuild as img
+   *  Renders if !Dweb: as <img/>
+   *    This image is positioned by container in top-left of Tile
+   *
+   * Containing component should make it clickable to navigate to parent.
+   *
+   */
 
-  //TODO-IAUX remove dependence on this functionality, have higher level component figure the parent identifier.
+  // TODO-IAUX remove dependence on this functionality, have higher level component figure the parent identifier.
 
   // Note this is only called in the dweb-archive/ReactFake case
   loadcallable(enclosingspan) { // Defined as a closure so that can access identifier (in real React), in ReactFake its called with ref.call(this,enclosingspan)
@@ -32,9 +38,9 @@ export default class ParentTileImg extends IAReactComponent {
     if (typeof DwebArchive !== 'undefined') {
       // TODO-DWEB build img processing from ReactFake into tile-tile-image and ParentTileImg but wait till have non-tile images as well
       // Returns <span> which ReactFake.loadImg fills in
-      return <span ref={this.load}></span>;
+      return <span ref={this.load} />;
     } else { // Pure IAUX
-      return <img src={`https://archive.org/services/img/${this.identifier}`}/>; // Note requires a way to determine parent collection identifier
+      return <img src={`https://archive.org/services/img/${this.identifier}`} alt={this.identifier} />; // Note requires a way to determine parent collection identifier
     }
   }
 }
