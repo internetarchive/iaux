546727f12ce7c98a37f8b3e36174c21bb6354955
Isa Herico-Velasco <isaherico@gmail.com>
2019-06-21 16:13:27 -0700

    Creating DWEB index file

--- packages/ia-components/index.js
+++ packages/ia-components/index.js
@@ -12,17 +12,3 @@ export { default as FlexboxPagination } from './sandbox/pagination/pagination-wi
 export { default as TheatreTrackList } from './sandbox/track-lists/track-list';
 export { default as TheatreAudioPlayer } from './sandbox/audio-players/audio-player-main';
 export { default as AudioPlayerWithYoutubeSpotify } from './sandbox/theatres/with-youtube-spotify/audio-with-youtube-spotify-main';
-export { default as AnchorDetails } from './sandbox/AnchorDetails';
-// sandbox tiles
-export { default as ParentTileImage } from './sandbox/tiles/ParentTileImg';
-export { default as RelatedItems } from './sandbox/tiles/RelatedItems';
-export { default as TileComponent } from './sandbox/tiles/TileComponent';
-export { TileGrid, ScrollableTileGrid } from './sandbox/tiles/TileGrid';
-// details page
-export { default as AnchorDownload } from './sandbox/details/AnchorDownload';
-export { default as DetailsCollectionList } from './sandbox/details/DetailsCollectionList';
-export { DetailsFlagLI, DetailsFlags, DetailsActionButtons } from './sandbox/details/DetailsActionButtons';
-export { default as DetailsDownloadOptions } from './sandbox/details/DetailsDownloadOptions';
-export { AnchorModalGo, ButtonModalGo } from './sandbox/details/ModalGo';
-export { default as Tabby } from './sandbox/details/Tabby';
-export {NavWrap} from './sandbox/details/NavWrap';
