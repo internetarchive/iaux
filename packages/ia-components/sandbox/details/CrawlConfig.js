b7f0a795c12d934d1039f8cf88b7a1b9f6dfce66
Mitra Ardron <mitra@mitra.biz>
2019-06-22 08:37:29 +1000

    Mitra crawlconfig (#202)
    * Crawl notification on TileComponent
    
    * uxlocal - handle new place for crawl in items
    
    * NavWrap partial
    
    * UXlocal - debugging crawl and transport buttons
    
    * Use gatewayServer in util.js
    
    * DetailsCollectionList to now show collection image (as on archive.org), catch edge case in TileComponent
    
    * Merge DetailsFlags into DetailsActionButtons and document component
    
    * Fix issue with new canonicalUrl and remove now unused DetailsFlags.jsx
    
    * Merge master resolve conflicts
    
    * Restore DetailsCollectionList without calls to expand titles, adds canoncialUrl and gatewayServer to util.js
    
    * Fix imports for iaux
    
    * Oops - imports should work now
    
    * Bug fixing in home and local
    
    * canonicalUrl fix for when offline
    
    * JSDocs on ConfigDetailsComponent
    
    * Added downloaded to ConfigDetailsComponent
    
    * Commented out broken IAUXExampleComponent while Isa fixes it
    
    * Change to commented out code calling obsolete functions
    
    * DetailsCollectionList jdocs
    
    * DetailsCollectionList jdocs
    
    * Debugging in util.js
    
    * Refactoring DetailsFlags DetailsActionButtons
    
    * Add NavWrap component
    
    * Minor fixes to AnchorDownload; ObjectFromEntries; ObjectFilter; ModalGo; TileComponent; TileGrid
    
    * Merge branch 'mitra-PRsubmitted' into mitra-uxlocal
    
    # Conflicts:
    #	packages/ia-components/index.js
    #	packages/ia-components/sandbox/tiles/TileComponent.jsx
    #	packages/ia-components/util.js
    
    * Oops - imported DetailsCollectionList twice into index.js
    
    * Rename ConfigDetailsComponent to ConfigCrawl
    
    * Oops
    
    * Reload button partial
    
    * Fix index
    
    * Refactor Related Items to remove dependency on dweb-archivecontroller
    
    * Reload button partial
    
    * Fix some "need a key" warnings from React
    
    * Nav buttons for local and settings
    
    * Fix bug in NavWrap search and reorganize reload button into DwebNavButtons
    
    * Make CrawlConfig empty if not running on mirror, and remove the hard-render as no longer needed
    
    * Switch require to import
    
    * Changes made by: yarn install; yarn run lerna bootstrap; yarn run lerna link;
    
    * Eslint enforced changes that add pretty much nothing to readability (and that's after the bug-introducing ones or ones that made it unreadable had been removed refused)
    
    * Trivial changes, tidy ups, and comments
    
    * Oops
    
    * Dweb : Pass wanthistory to Navigation
    
    * 168: Uxlocal fix conflict
    
    * 168-uxlocal fix remaining conflicts
    
    * Just documentation and code cleanup
    
    * Just documentation and code cleanup/linting
    
    * Just documentation and code cleanup / linting
    
    * Fix issues with DownloadOptions and the Download Directory it goes to
    
    * Crawl Config to have size indicator (partial)
    
    * Crawl Config - support expanded metadata about downloads on Tiles and in CrawlConfig
    
    * Remove obsolete and commented out code and Lint

--- packages/ia-components/sandbox/details/CrawlConfig.js
+++ packages/ia-components/sandbox/details/CrawlConfig.js
@@ -1,6 +1,7 @@
 // DwebTransports is not needed, its a global
 import canonicaljson from '@stratumn/canonicaljson';
 import React from 'react';
+import prettierBytes from 'prettier-bytes';
 import IAReactComponent from '../IAReactComponent';
 import { gatewayServer } from '../../util';
 
@@ -65,53 +66,25 @@ export default class CrawlConfig extends IAReactComponent {
       return null;
     }
     const className = `crawl${this.state.level ? this.state.level : this.state.downloaded ? 'downloaded' : 'none'}`;
+    const isDownloaded = this.state.downloaded && this.state.downloaded.details;
+    const dl = this.state.downloaded; // Speed up access below
     return (
       <ul>
         <li className={className} data-id={this.props.identifier} key={this.props.identifier} onClick={this.onClick}>
-          {this.state.level ? `Crawling ${this.state.level}` : this.state.downloaded ? 'Downloaded' : 'Not Crawling'}
+          <span>{this.state.level ? `Crawling ${this.state.level}` : isDownloaded ? 'Downloaded' : 'Not Downloaded'} </span>
+          {!dl ? null
+            : dl.members_all_count
+              ? <span>{`${prettierBytes(dl.members_size)} in ${dl.members_details_count} of ${dl.members_all_count} items`}</span>
+              : <span>{prettierBytes(dl.files_size) + " / " + prettierBytes(dl.files_all_size)} </span>
+          }
           { (this.state.search && CrawlConfig._levels.indexOf(this.state.level) >= CrawlConfig._levels.indexOf('details'))
-            ? <span>{`  Search ${this.state.search.rows} rows at ${this.state.search.level}`}</span>
+            ? <span>{`  Searching ${this.state.search.rows} rows at ${this.state.search.level}`}</span>
             : null }
         </li>
       </ul>
     );
   }
 
-  /*
-    loadcallable(enclosingEl) {
-        // Called by React when the Loading... div is displayed
-        const urlConfig = [gatewayServer(), "info"].join('/');
-        this.enclosingElement = enclosingEl; // Tell it where to render inside when info found
-        DwebTransports.httptools.p_GET(urlConfig, {}, (err, info) => {
-            if (err) {
-                debug("Config Failed to get info");
-            } else {
-                this.stateFromInfo(info, (err, res) => this.setState(res));
-            }
-        });
-    }
-    stateFromInfo(info, cb) {
-        const identifier = this.props.identifier;
-        const config = info.config; // Mixed in with other info
-        const configdefault = config[0];
-        const configuser = config[1] || {};
-        const configmerged = ObjectDeeperAssign({}, configdefault, configuser); // Cheating, but assumes no arrays needing merging
-        // noinspection JSUnresolvedVariable
-        // Note there is similar code in dweb-mirror.MirrorConfig.crawlMember
-        const task = configmerged.apps.crawl.tasks.find(t => t.identifier.includes(identifier));
-        const isDetailsOrMore = task && CrawlConfig._levels.indexOf(task.level) >= CrawlConfig._levels.indexOf("details");
-        // noinspection JSUnresolvedVariable
-        const search = task && ( task.search  || (isDetailsOrMore && configmerged.apps.crawl.opts.defaultDetailsSearch));
-        // noinspection JSUnusedGlobalSymbols
-        cb(null, {
-            task, configdefault, configuser, configmerged,
-            oldhash: info.hash,
-            level: task && task.level,
-            rows: search && search.rows,
-            searchLevel: search && search.level
-        });
-    }
-    */
   clickCallable() {
     // Cycle through possible states on click
     debug('%s: Crawl clicked', this.state.identifier);
