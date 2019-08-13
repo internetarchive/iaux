7b23f9c70d004f98a7c349154e3c30bdb1c10d7c
Shane Riley <shane@shaneriley.com>
2019-08-13 11:10:11 -0400

    Adds Selenium Webdriver

---
+++ packages/ia-components/integration/example.test.js
@@ -0,0 +1,58 @@
+import { Builder, By, until } from 'selenium-webdriver';
+import chrome from 'selenium-webdriver/chrome';
+// Uncomment to use Firefox (note that Firefox is more flaky):
+// import firefox from 'selenium-webdriver/firefox';
+
+// Example config to use BrowserStack
+//
+// const capabilities = {
+//   'browserName' : 'Firefox',
+//   'browser_version' : '67.0 beta',
+//   'os' : 'OS X',
+//   'os_version' : 'Mojave',
+//   'resolution' : '1024x768',
+//   'browserstack.user' : 'internetarchive1',
+//   'browserstack.key' : 'REPLACE_WITH_KEY',
+//   'name' : 'Sample Test'
+// }
+// var driver = new webdriver.Builder()
+//   .usingServer('http://hub-cloud.browserstack.com/wd/hub')
+//   .withCapabilities(capabilities)
+//   .build();
+
+describe('Example: Test search engine', () => {
+  let driver;
+
+  beforeAll(async () => {
+    // For Firefox, change "chrome" to "firefox" and "Chrome" to "Firefox"
+    // everywhere below
+    const options = new chrome.Options();
+
+    // Uncomment to go headless:
+    // options.headless();
+
+    driver = new Builder()
+      .forBrowser('chrome')
+      .setChromeOptions(options)
+      .build();
+  });
+
+  afterAll(async () => {
+    await driver.quit();
+  });
+
+  test('we can access Google', async () => {
+    await driver.get('http://www.google.com');
+    await driver.findElement(By.name('q')).sendKeys('BrowserStack');
+
+    // Because the Search button only appears later and needs to be scrolled to,
+    // we first locate the button, then wait until it's visible.
+    // NOTE: `elementIsVisible()` takes an element, not a locator.
+    const button = await driver.wait(until.elementLocated(By.name('btnK')));
+    await driver.wait(until.elementIsVisible(button)).click();
+
+    const targetElement = await driver.findElement(By.css('[data-attrid="visit_official_site"]'));
+    const elementText = await targetElement.getText();
+    return expect(elementText).toEqual('browserstack.com');
+  });
+});
