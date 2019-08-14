abde30e6409205273b2fd1f514245cba658dcd2f
Shane Riley <shane@shaneriley.com>
2019-08-14 05:22:40 -0400

    Moves Selenium config files to non-ignored path

---
+++ packages/ia-components/integration/jestConfigAfterEnv.js
@@ -0,0 +1,3 @@
+// Jest times out if execution of its tests takes too long, where "too long" is 5 seconds by default.
+// Browser tests usually take longer, so this increases the time to 20 seconds.
+jest.setTimeout(20000);
