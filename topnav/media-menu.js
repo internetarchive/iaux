d1ec6c53b6aae4363c3c80d7072f4a5d24b352a2
Koder Joker <koderjoker@gmail.com>
2019-08-30 21:28:03 +0530

    Implement css grid

--- topnav/media-menu.js
+++ topnav/media-menu.js
@@ -9,17 +9,24 @@ class MediaMenu extends LitElement {
 
   render() {
     return html`
-      <nav
-        class="media-menu"
-      >
-        <div><a href="#">Wayback Machine</a></div>
-        <div><a href="#">Texts</a></div>
-        <div><a href="#">Video</a></div>
-        <div><a href="#">Audio</a></div>
-        <div><a href="#">Software</a></div>
-        <div><a href="#">Images</a></div>
-        <div><a href="#">More</a></div>
-      </nav>
+    <nav class="media-menu">
+      <div class="media-menu">
+        <a href="#"><div><img src="assets/img/ia-waybackmachine-999.svg"></div></a>
+        <a href="#"><div>Wayback Machine</div></a>
+        <div><img src="assets/img/ia-texts-999.svg"></div>
+        <div>Texts</div>
+        <div><img src="assets/img/ia-video-999.svg"></div>
+        <div>Video</div>
+        <div><img src="assets/img/ia-audio-999.svg"></div>
+        <div>Audio</div>
+        <div><img src="assets/img/ia-software-999.svg"></div>
+        <div>Software</div>
+        <div><img src="assets/img/ia-images-999.svg"></div>
+        <div>Images</div>
+        <div><img src="assets/img/ia-more-999.svg"></div>
+        <div>More</div>
+      </div>
+    </nav>
     `;
   }
 
@@ -29,6 +36,21 @@ class MediaMenu extends LitElement {
         width: 100%;
         height: 500px;
         background-color: var(--black);
+        color: var(--white);
+        margin: 0px;
+        font-size: 20px;
+        display: grid;
+        grid-template-columns: 50px 300px;
+        font-family: "Helvetica Neue";
+      }
+      .media-menu div {
+        height: 40px;
+        padding: 10px;
+      }
+      a {
+        display: inline;
+        color: var(--white);
+        text-decoration: none;
       }
      `;
   }
