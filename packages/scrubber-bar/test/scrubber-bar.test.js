59765a81cda6bdad7a2dc7e212d1e21e6ae53c8e
Jason Buckner <jasonb@archive.org>
2019-09-10 13:55:36 -0700

    Add tests

--- packages/scrubber-bar/test/scrubber-bar.test.js
+++ packages/scrubber-bar/test/scrubber-bar.test.js
@@ -1,4 +1,4 @@
-import { html, fixture, expect } from '@open-wc/testing';
+import { html, fixture, expect, oneEvent } from '@open-wc/testing';
 
 import '../scrubber-bar';
 
@@ -10,4 +10,54 @@ describe('ScrubberBar', () => {
 
     expect(el.percentComplete).to.equal(0);
   });
+
+  it('userInteracting flag gets set to true when mousedown on slider', async () => {
+    const el = await fixture(html`
+      <scrubber-bar></scrubber-bar>
+    `);
+
+    expect(el.userInteracting).to.equal(false);
+
+    const rangeSlider = el.shadowRoot.getElementById('slider');
+    const mouseDownEvent = new MouseEvent('mousedown');
+    rangeSlider.dispatchEvent(mouseDownEvent)
+
+    expect(el.userInteracting).to.equal(true);
+  });
+
+  it('userInteracting flag gets set to false when mouseup on slider', async () => {
+    const el = await fixture(html`
+      <scrubber-bar></scrubber-bar>
+    `);
+
+    expect(el.userInteracting).to.equal(false);
+
+    const rangeSlider = el.shadowRoot.getElementById('slider');
+    const mouseDownEvent = new MouseEvent('mousedown');
+    rangeSlider.dispatchEvent(mouseDownEvent)
+
+    expect(el.userInteracting).to.equal(true);
+
+    const mouseUpEvent = new MouseEvent('mouseup');
+    rangeSlider.dispatchEvent(mouseUpEvent)
+
+    expect(el.userInteracting).to.equal(false);
+  });
+
+  it('valuechange event is emitted when input occures on slider', async () => {
+    const el = await fixture(html`
+      <scrubber-bar></scrubber-bar>
+    `);
+
+    const rangeSlider = el.shadowRoot.getElementById('slider');
+    const inputEvent = new Event('input');
+
+    // we have to do this in a setTimeout so the event listener below has a chance to listen
+    setTimeout(() => { rangeSlider.dispatchEvent(inputEvent); });
+
+    const { detail } = await oneEvent(el, 'valuechange');
+    expect(detail.value).to.equal("0");
+
+  });
+
 });
