5c898dc3ccb5c6b9be5dec981e76408560b8ccff
Jason Buckner <jason@jasonbuckner.com>
2019-09-16 13:08:51 -0700

    Remove console logging

--- packages/waveform-progress/src/waveform-progress.ts
+++ packages/waveform-progress/src/waveform-progress.ts
@@ -24,7 +24,6 @@ export default class WaveformProgress extends LitElement {
   private _userIsInteracting = false;
 
   render() {
-    console.log('INTERACTIVE', this.interactive);
     return html`
       <div class="container">
         <div id="fill" style="width: ${this._percentComplete}%"></div>
@@ -52,7 +51,6 @@ export default class WaveformProgress extends LitElement {
   }
 
   updated(changedProperties: PropertyValues): void {
-    console.log('updated', changedProperties);
     if (!changedProperties.has('percentComplete') || this._userIsInteracting) {
       return;
     }
@@ -61,19 +59,16 @@ export default class WaveformProgress extends LitElement {
   }
 
   private drag(e: MouseEvent) {
-    console.log('drag', e);
     if (!this._userIsInteracting) { return; }
     this._percentComplete = this.offsetXToPercent(e.offsetX);
     this.dispatchValueChangeEvent()
   }
 
   private dragstart(e: Event) {
-    console.log('dragstart', e);
     this._userIsInteracting = true;
   }
 
   private dragend(e: Event) {
-    console.log('dragend', e);
     this._userIsInteracting = false;
   }
 
@@ -94,7 +89,6 @@ export default class WaveformProgress extends LitElement {
     if (this.dragcover === null) { return 0; }
     const width: number = this.dragcover.clientWidth;
     const percentComplete: number = offsetX / width * 100;
-    console.log('offsetXToPercent', width, offsetX, percentComplete);
     return percentComplete;
   }
 
