9a087c584e20f8e25794e2797f26f19ed0491492
Jason Buckner <jasonb@archive.org>
2019-09-24 11:17:08 -0700

    Add sample transcript

---
+++ packages/transcript-view/demo/transcript.js
@@ -0,0 +1,67 @@
+const transcript = [
+  {
+      "id": 1,
+      "start": 4.058,
+      "end": 6.032,
+      "text": "Hello I'm Steve Miller U.S.",
+      "is_music": false
+  },
+  {
+      "id": 2,
+      "start": 6.033,
+      "end": 10.029,
+      "text": "President Donald Trump blame Congress Thursday for creating new tensions with",
+      "is_music": false
+  },
+  {
+      "id": 3,
+      "start": 10.03,
+      "end": 14.093,
+      "text": "Russia by approving sanctions against Moscow and the Kremlin agreed that penalties",
+      "is_music": false
+  },
+  {
+      "id": 4,
+      "start": 14.094,
+      "end": 19.098,
+      "text": "would thwart improved relations and you pal cesky has more the president said that",
+      "is_music": false
+  },
+  {
+      "id": 5,
+      "start": 19.099,
+      "end": 20.037,
+      "text": "the U.S.",
+      "is_music": false
+  },
+  {
+      "id": 6,
+      "start": 20.038,
+      "end": 24.065,
+      "text": "Relationship with Russia is at an all time and very dangerous low and said quote",
+      "is_music": false
+  },
+  {
+      "id": 7,
+      "start": 24.069,
+      "end": 36.03,
+      "text": "",
+      "is_music": true
+  },
+  {
+      "id": 10,
+      "start": 36.047,
+      "end": 40.057,
+      "text": "said Trump signing of the package of new Russia sanctions ends hopes for improving",
+      "is_music": false
+  },
+  {
+      "id": 11,
+      "start": 40.058,
+      "end": 43.051,
+      "text": "our relations and repel cesky V.O.A.",
+      "is_music": false
+  }
+]
+
+export default transcript;
