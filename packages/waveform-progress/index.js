5f3019d1a184df3bb8c3f11d4d7882e0b811d6f9
Jason Buckner <jasonb@archive.org>
2019-10-08 16:53:08 -0700

    Update some configurations to help with TypeScript support

--- packages/waveform-progress/index.js
+++ packages/waveform-progress/index.js
@@ -1,3 +1,4 @@
 import WaveformProgress from './lib/waveform-progress.js';
+import ZoneOfSilence from './lib/models/zone-of-silence.js';
 
-export default WaveformProgress;
+export { WaveformProgress, ZoneOfSilence };
