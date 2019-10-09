5aa01f79c0896130722ed5fbdae1d2227f0da790
Jason Buckner <jason@jasonbuckner.com>
2019-10-09 16:33:50 -0700

    Export all dependencies of the radio-player

--- packages/radio-player/index.js
+++ packages/radio-player/index.js
@@ -1,3 +1,7 @@
 import RadioPlayer from './lib/src/radio-player.js';
+import RadioPlayerConfig from './lib/src/models/radio-player-config.js';
 
-export default RadioPlayer;
+import { TranscriptConfig, TranscriptEntryConfig } from '@internetarchive/transcript-view';
+import { AudioSource } from '@internetarchive/audio-element';
+
+export { RadioPlayer, RadioPlayerConfig, TranscriptConfig, TranscriptEntryConfig, AudioSource };
