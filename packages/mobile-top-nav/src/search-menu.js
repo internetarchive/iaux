8475a9c1f4a622024428917ca21cf6a910391239
Internet Archive <archive@internets-air-4.lan>
2019-10-09 22:39:11 -0700

    Simplify DOM & CSS - use only elements that we absolutely need - simplify element animation to mainly use css transitions

--- packages/mobile-top-nav/src/search-menu.js
+++ packages/mobile-top-nav/src/search-menu.js
@@ -4,50 +4,58 @@ class SearchMenu extends LitElement {
   static get properties() {
     return {
       searchMenuOpen: { type: Boolean },
-      searchMenuAnimate: { type: Boolean }
+      searchMenuAnimate: { type: Boolean },
     };
   }
 
   render() {
-    const searchMenuClass = this.searchMenuOpen ? 'search-menu open slide-in' : this.searchMenuAnimate ? 'search-menu slide-out' : 'search-menu';
-    const searchMenuHidden = this.searchMenuOpen ? 'false' : 'true';
-    const searchMenuExpanded = this.searchMenuOpen ? 'true' : 'false';
+    let searchMenuClass = 'initial';
+    if (this.searchMenuOpen) {
+      searchMenuClass = 'open';
+    }
+    if (!this.searchMenuOpen && this.searchMenuAnimate) {
+      searchMenuClass = 'closed';
+    }
+
+    const searchMenuHidden = Boolean(!this.searchMenuOpen).toString();
+    const searchMenuExpanded = Boolean(this.searchMenuOpen).toString();
+
     return html`
-    <div
-      class="${searchMenuClass}"
-      aria-hidden="${searchMenuHidden}"
-      aria-expanded="${searchMenuExpanded}"
-    >
-      <div class="search-options">
-        <div>
-          <label>
-            <input type="radio" name="search" value="metadata" checked>
-            Search Metadata
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="radio" name="search" value="text-contents">
-            Search text contents
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="radio" name="search" value="tv-news-captions">
-            Search TV news captions
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="radio" name="search" value="archived-websites">
-            Search archived websites
-          </label>
-        </div>
-        <div>
-          <a href="#">Advanced Search</a>
+      <div
+        class="search-menu tx-slide ${searchMenuClass}"
+        aria-hidden="${searchMenuHidden}"
+        aria-expanded="${searchMenuExpanded}"
+      >
+        <div class="search-options">
+          <div>
+            <label>
+              <input type="radio" name="search" value="metadata" checked />
+              Search Metadata
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="radio" name="search" value="text-contents" />
+              Search text contents
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="radio" name="search" value="tv-news-captions" />
+              Search TV news captions
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="radio" name="search" value="archived-websites" />
+              Search archived websites
+            </label>
+          </div>
+          <div>
+            <a href="#">Advanced Search</a>
+          </div>
         </div>
       </div>
-    </div>
     `;
   }
 
@@ -55,44 +63,33 @@ class SearchMenu extends LitElement {
     return css`
       .search-menu {
         position: relative;
-        display: flex;
-        flex-direction: column;
         margin: 0;
         width: 100%;
-        height: 200px;
         font-size: 20px;
         background-color: var(--grey20);
-        padding: 10px;
-        transform: translate(0, -1500px);
+        display: flex;
+        flex-direction: column;
       }
-      .open {
-        transform: translate(0, -1065px);
+      .search-menu.tx-slide {
+        overflow: hidden;
+        transition-property: max-height;
+        transition-duration: 1.5s;
+        transition-timing-function: ease;
+      }
+      .search-menu.tx-slide.initial,
+      .search-menu.tx-slide.closed {
+        max-height: 0;
+      }
+      .search-menu.tx-slide.closed {
+        transition-duration: 0.1s;
+      }
+      .search-menu.tx-slide.open {
+        max-height: 100vh;
         z-index: 3;
-      }
-      @keyframes slide-in {
-        0% {
-          transform: translate(0, -1500px);
-        }
-        100% {
-          transform: translate(0, -1065px);
-        }
-      }
-      @keyframes slide-out {
-        0% {
-          transform: translate(0, -1065px);
-        }
-        100% {
-          transform: translate(0, -1500px);
-        }
-      }
-      .slide-in {
-        animation: slide-in 0.5s forwards;
-      }
-      .slide-out {
-        animation: slide-out 0.5s forwards;
       }
       .search-options {
         align-self: center;
+        margin: 1% auto;
       }
       .search-options div {
         padding: 8px;
@@ -103,7 +100,7 @@ class SearchMenu extends LitElement {
         color: var(--link-color);
         text-decoration: none;
       }
-     `;
+    `;
   }
 }
 
