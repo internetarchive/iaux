9887531cad226d14d05b951a985ded1c457ca8b6
Jason Buckner <jason@jasonbuckner.com>
2019-10-16 12:51:05 -0700

    Clean up imports

--- packages/radio-player/index.js
+++ packages/radio-player/index.js
@@ -1,7 +1,16 @@
 import RadioPlayer from './lib/src/radio-player.js';
 import RadioPlayerConfig from './lib/src/models/radio-player-config.js';
 
-import { TranscriptConfig, TranscriptEntryConfig } from '@internetarchive/transcript-view';
+import {
+  TranscriptConfig,
+  TranscriptEntryConfig
+} from '@internetarchive/transcript-view';
 import { AudioSource } from '@internetarchive/audio-element';
 
-export { RadioPlayer, RadioPlayerConfig, TranscriptConfig, TranscriptEntryConfig, AudioSource };
+export {
+  RadioPlayer,
+  RadioPlayerConfig,
+  TranscriptConfig,
+  TranscriptEntryConfig,
+  AudioSource
+};
