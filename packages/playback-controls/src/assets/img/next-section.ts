cdd038fff8a8da2463592334ec7d8d7dd12e7435
Jason Buckner <jasonb@archive.org>
2019-10-21 14:59:02 -0700

    Update next/prev section button images

--- packages/playback-controls/src/assets/img/next-section.ts
+++ packages/playback-controls/src/assets/img/next-section.ts
@@ -1,7 +1,12 @@
 import { html } from 'lit-html';
 
 const image = html`
-<svg height="20" viewBox="0 0 18 20" width="18" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="m8 2 10 16h-20z" fill="#fff" transform="matrix(0 1 -1 0 18 2)"/><path d="m17 20v-20" stroke="#fff"/></g></svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" viewBox="0 0 18 20">
+  <g fill="none" fill-rule="evenodd" transform="translate(1)">
+    <polygon fill="#FFF" points="7 3 16 17 -2 17" transform="rotate(90 7 10)"/>
+    <line x1="15" x2="15" y1="20" stroke="#FFF" stroke-width="2"/>
+  </g>
+</svg>
 `;
 
 export default image;
--- packages/playback-controls/src/assets/img/previous-section.ts
+++ packages/playback-controls/src/assets/img/previous-section.ts
@@ -1,7 +1,12 @@
 import { html } from 'lit-html';
 
 const image = html`
-<svg height="20" viewBox="0 0 18 20" width="18" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="m10 2 10 16h-20z" fill="#fff" transform="matrix(0 -1 1 0 0 20)"/><path d="m1 20v-20" stroke="#fff"/></g></svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" viewBox="0 0 18 20">
+  <g fill="none" fill-rule="evenodd" transform="translate(1 1)">
+    <polygon fill="#FFF" points="9 2 18 16 0 16" transform="rotate(-90 9 9)"/>
+    <line x1=".5" x2=".5" y1="18" stroke="#FFF" stroke-width="2"/>
+  </g>
+</svg>
 `;
 
 export default image;
