7c90baf5fec63d78b1c8f75aac3fd3e3679abc9d
Shane Riley <shane@shaneriley.com>
2019-10-29 12:59:29 -0400

    Creates ia-components release script

---
+++ packages/ia-components/scripts/version.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const { execSync } = require('child_process');
+const { version: NEW_VERSION } = require('../package.json');
+
+async function main() {
+  // Update the changelog
+  const old_changelog = fs.readFileSync('CHANGELOG.md').toString();
+  const new_changelog = `# ${NEW_VERSION}\n<!-- Write here... -->\n\n` + old_changelog;
+  fs.writeFileSync('CHANGELOG.md', new_changelog);
+  while (fs.readFileSync('CHANGELOG.md').toString().includes('<!-- Write here... -->')) {
+    console.log('Changelog contains dummy text. Update the changelog, and press ENTER continue.');
+    await new Promise(res => process.stdin.once("data", res));
+  }
+  execSync('git add CHANGELOG.md');
+}
+
+main().then(() => process.exit());
