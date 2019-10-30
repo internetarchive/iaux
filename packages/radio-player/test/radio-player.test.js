cbca52af926db17a5916a09b637fa987df1b0e77
Jason Buckner <jason@jasonbuckner.com>
2019-10-30 16:47:51 -0700

    Add some more tests

--- packages/radio-player/test/radio-player.test.js
+++ packages/radio-player/test/radio-player.test.js
@@ -5,8 +5,10 @@ import {
 import promisedSleep from './promised-sleep';
 
 import '../lib/src/radio-player';
-import { AudioSource } from '../index';
+import { AudioSource, TranscriptEntryConfig } from '../index';
 import RadioPlayerConfig from '../lib/src/models/radio-player-config';
+import sampleTranscript from './sample_transcript';
+import { TranscriptConfig } from '@internetarchive/transcript-view';
 
 describe('Radio Player', () => {
   it('has no configuration', async () => {
@@ -95,4 +97,80 @@ describe('Radio Player', () => {
     await promisedSleep(250);  // give it time to seek
     expect(el.currentTime).to.equal(0.7);
   });
+
+  it('generates proper scrubber bar section markers', async () => {
+    const entry1 = new TranscriptEntryConfig(1, 1, 17, 'foo', false);
+    const entry2 = new TranscriptEntryConfig(1, 18, 37, '', true);
+    const entry3 = new TranscriptEntryConfig(1, 37, 56, 'bar', false);
+    const entry4 = new TranscriptEntryConfig(1, 57, 74, 'baz', true);
+    const entry5 = new TranscriptEntryConfig(1, 75, 100, 'baz', false);
+    const entries = [entry1, entry2, entry3, entry4, entry5];
+
+    const transcriptConfig = new TranscriptConfig(entries);
+
+    const el = await fixture(html`
+      <radio-player .transcriptConfig=${transcriptConfig}></radio-player>
+    `);
+
+    el.duration = 100
+
+    const expected = [0, 18, 37, 57, 74, 100];
+
+    el.scrubberBarMarkerPercentages.forEach((element, index) => {
+      // we need to round the values because sometimes the percentage calculations
+      // come out to 56.9999999
+      expect(Math.round(element)).to.equal(expected[index]);
+    });
+  });
+
+  it('shows and hides the search results switcher', async () => {
+    const el = await fixture(html`
+      <radio-player></radio-player>
+    `);
+
+    const searchResultsSwitcher = el.shadowRoot.querySelector('search-results-switcher');
+    expect(searchResultsSwitcher.classList.contains('hidden')).to.equal(true);
+
+    el.shouldShowSearchResultSwitcher = true;
+    await promisedSleep(100);
+    expect(searchResultsSwitcher.classList.contains('hidden')).to.equal(false);
+  });
+
+  it('shows and hides the no search results message', async () => {
+    const el = await fixture(html`
+      <radio-player></radio-player>
+    `);
+
+    const searchResultsSwitcher = el.shadowRoot.querySelector('.no-search-results-message');
+    expect(searchResultsSwitcher.classList.contains('hidden')).to.equal(true);
+
+    el.shouldShowNoSearchResultMessage = true;
+    await promisedSleep(100);
+    expect(searchResultsSwitcher.classList.contains('hidden')).to.equal(false);
+  });
+
+  // it('updates the search term when updateSearchTerm event is emitted', async () => {
+  //   const el = await fixture(html`
+  //     <radio-player></radio-player>
+  //   `);
+
+  //   const searchBar = el.shadowRoot.querySelector('expandable-search-bar');
+  //   const inputField = searchBar.shadowRoot.querySelector('input[type=text]');
+  //   const keyUpEvent = new KeyboardEvent('keyup', { key: 'A' });
+
+  //   // searchBar.value = 'foo';
+
+  //   inputField.dispatchEvent(keyUpEvent);
+
+  //   await promisedSleep(100);
+
+  //   expect(el.searchTerm).to.equal('A');
+
+  //   // we have to do this in a setTimeout so the event listener below has a chance to listen
+  //   // setTimeout(() => { searchBar.value = 'foo'; });
+  //   // const response = await oneEvent(el, 'playbackPaused');
+  //   // expect(response).to.exist;
+  // });
+
+
 });
