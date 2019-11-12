10715a497858b5d42e9ec0c3eaf06b84993c41de
Jason Buckner <jasonb@archive.org>
2019-11-12 13:05:40 -0800

    Radio Player Component (#349)
    Add the main Radio Player component that orchestrates all of the other components to make the whole radio player work.

---
+++ packages/radio-player/index.d.ts
@@ -0,0 +1,7 @@
+import RadioPlayer from './lib/src/radio-player.d';
+import SearchResultsSwitcher from './lib/src/search-results-switcher.d';
+import SearchBar from './lib/src/search-bar/search-bar.d';
+import MusicZone from './lib/src/models/music-zone.d';
+import RadioPlayerConfig from './lib/src/models/radio-player-config.d';
+
+export { RadioPlayer, SearchResultsSwitcher, SearchBar, MusicZone, RadioPlayerConfig };
