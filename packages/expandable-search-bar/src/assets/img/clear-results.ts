78211c40bd56371f12531541fdddcd3de92d3888
Jason Buckner <jasonb@archive.org>
2019-11-12 11:10:52 -0800

    Add title tags to the svg buttons for accessibility

--- packages/expandable-search-bar/src/assets/img/clear-results.ts
+++ packages/expandable-search-bar/src/assets/img/clear-results.ts
@@ -2,6 +2,7 @@ import { html } from 'lit-html';
 
 const image = html`
   <svg height="12" viewBox="0 0 12 12" width="12" xmlns="http://www.w3.org/2000/svg">
+    <title>Clear Search Results</title>
     <g fill="none" fill-rule="evenodd">
       <circle cx="6" cy="6" fill="#fff" r="6" />
       <g stroke="#000" stroke-linecap="round">
--- packages/expandable-search-bar/src/assets/img/disclosure.ts
+++ packages/expandable-search-bar/src/assets/img/disclosure.ts
@@ -2,6 +2,7 @@ import { html } from 'lit-html';
 
 const image = html`
   <svg height="9" viewBox="0 0 19 9" width="19" xmlns="http://www.w3.org/2000/svg">
+    <title>Toggle Quick Search</title>
     <path
       d="m1 1 9 7 8-7"
       fill="none"
