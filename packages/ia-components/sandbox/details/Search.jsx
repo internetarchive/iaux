a92fe5dfcd9a9dc0a491723c5123c28637322367
Mitra Ardron <mitra@mitra.biz>
2019-12-11 12:10:55 +1100

    SaveSearch code review

--- packages/ia-components/sandbox/details/Search.jsx
+++ packages/ia-components/sandbox/details/Search.jsx
@@ -1,10 +1,12 @@
+/* eslint-disable max-len, import/prefer-default-export */
+/* eslint-disable react/jsx-one-expression-per-line, react/destructuring-assignment, react/prefer-stateless-function, react/prop-types  */
 // const debug = require('debug')('dweb-archive:SearchSwitcher');
+
 import React from 'react';
-import IAReactComponent from '../IAReactComponent';
 import { AnchorSearch } from './AnchorSearch';
 import { I18nSpan } from '../languages/Languages';
 
-class SearchSwitcher extends IAReactComponent {
+class SearchSwitcher extends React.Component {
 /**
  * <SearchSwitcher
  *    identifier: identifier of collection, undefined for searches
@@ -19,26 +21,26 @@ class SearchSwitcher extends IAReactComponent {
       <div className="topinblock">
         <div className="hidden-xs hidden-sm">
           <div className="sort-by">
-            <I18nSpan en="SORT BY"/>
+            <I18nSpan en="SORT BY" />
           </div>
-          {/*--TODO-DETAILS this dropdown doesnt reorder, test other UI elements in vicinity as well see https://github.com/internetarchive/dweb-archive/issues/15--*/}
-          <span className="big-label blue-pop">{/*TODO-ISSUE dweb-archive#57 remove relevance on Collections*/}
-            {(!this.props.identifier) ? // Dont show on collections
-              <AnchorSearch className="ikind stealth in" data-id="relevance" query={this.props.query}><I18nSpan en="RELEVANCE"/></AnchorSearch>
+          {/* --TODO-DETAILS this dropdown doesnt reorder, test other UI elements in vicinity as well see https://github.com/internetarchive/dweb-archive/issues/15--*/}
+          <span className="big-label blue-pop">{/* TODO-ISSUE dweb-archive#57 remove relevance on Collections */}
+            {(!this.props.identifier) // Dont show on collections
+              ? <AnchorSearch className="ikind stealth in" data-id="relevance" query={this.props.query}><I18nSpan en="RELEVANCE" /></AnchorSearch>
               : undefined}
-            {(!this.props.identifier) ? // Dont show on collections
-              <div className="iconochive-dot ikind-sep"></div>
+            {(!this.props.identifier) // Dont show on collections
+              ? <div className="iconochive-dot ikind-sep" />
               : undefined}
             <AnchorSearch className="ikind stealth" data-id="views" query={this.props.query} sort="-downloads">VIEWS</AnchorSearch>
-            <div className="iconochive-dot ikind-sep"></div>
+            <div className="iconochive-dot ikind-sep" />
             <AnchorSearch className="ikind stealth" data-id="title" query={this.props.query} sort="titleSorter">TITLE</AnchorSearch>
-            <div className="iconochive-dot ikind-sep"></div>
+            <div className="iconochive-dot ikind-sep" />
             <AnchorSearch className="ikind stealth" id="date_switcher" data-id="date-archived" query={this.props.query} sort="-publicdate">DATE ARCHIVED</AnchorSearch>
-            <div className="iconochive-dot ikind-sep hidden"></div>
+            <div className="iconochive-dot ikind-sep hidden" />
             <AnchorSearch className="ikind stealth hidden" data-id="date-published" query={this.props.query} sort="-date">DATE PUBLISHED</AnchorSearch>
-            <div className="iconochive-dot ikind-sep hidden"></div>
+            <div className="iconochive-dot ikind-sep hidden" />
             <AnchorSearch className="ikind stealth hidden" data-id="date-reviewed" query={this.props.query} sort="-reviewdate">DATE REVIEWED</AnchorSearch>
-            <div className="iconochive-dot ikind-sep"></div>
+            <div className="iconochive-dot ikind-sep" />
             <AnchorSearch className="ikind stealth" data-id="creator" query={this.props.query} sort="creatorSorter">CREATOR</AnchorSearch>
           </span>
         </div>
@@ -46,4 +48,5 @@ class SearchSwitcher extends IAReactComponent {
     );
   }
 }
-export {SearchSwitcher}
+export { SearchSwitcher };
+/* Code review Mitra 2019-12-11 excluding HTML comparisom */
