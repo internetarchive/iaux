f7dc334d91f4532694c6be1422c22ad14b9f5913
Jason Buckner <jasonb@archive.org>
2020-01-22 10:18:43 -0800

    Waveform Progress Fixes and Linting (#340)
    Fix tests and linting in the Waveform Progress component

--- packages/waveform-progress/src/waveform-progress.ts
+++ packages/waveform-progress/src/waveform-progress.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-underscore-dangle */
+
 import {
   LitElement,
   html,
@@ -6,14 +8,18 @@ import {
   property,
   PropertyValues,
   TemplateResult,
+  CSSResult,
 } from 'lit-element';
 import ZoneOfSilence from './models/zone-of-silence';
 
 @customElement('waveform-progress')
 export default class WaveformProgress extends LitElement {
   @property({ type: Number }) percentComplete = 0;
+
   @property({ type: String }) waveformUrl = '';
+
   @property({ type: Boolean }) interactive = false;
+
   @property({ type: Array }) zonesOfSilence: ZoneOfSilence[] = [];
 
   // This is our internal, canonical source for the `percentComplete`.
@@ -22,45 +28,43 @@ export default class WaveformProgress extends LitElement {
   // we need to be able to control when that value gets updated
   @property({ type: Number }) private _percentComplete = 0;
 
-  private _userIsInteracting: boolean = false;
+  private _userIsInteracting = false;
 
-  render() {
+  render(): TemplateResult {
     return html`
       <div class="container">
         <div id="fill" style="width: ${this._percentComplete}%"></div>
-        <img class="waveform-image" src="${this.waveformUrl}">
-        ${this.zonesOfSilenceTemplate}
-        ${this.interactive ? this.interactionCoverTemplate : ''}
+        <img class="waveform-image" src="${this.waveformUrl}" />
+        ${this.zonesOfSilenceTemplate} ${this.interactive ? this.interactionCoverTemplate : ''}
       </div>
-      `;
+    `;
   }
 
   private get zonesOfSilenceTemplate(): TemplateResult {
     return html`
-      ${this.zonesOfSilence.map((zone: ZoneOfSilence) => {
-        return html`
+      ${this.zonesOfSilence.map(
+        (zone: ZoneOfSilence) => html`
           <div
             class="zone-of-silence"
-            style="left: ${zone.startPercent}%; width: ${zone.endPercent - zone.startPercent}%"></div>
-        `;
-      })}
-    `
+            style="left: ${zone.startPercent}%; width: ${zone.endPercent - zone.startPercent}%"
+          ></div>
+        `,
+      )}
+    `;
   }
 
   private get interactionCoverTemplate(): TemplateResult {
     return html`
       <div
         id="dragcover"
-
         @mousedown=${this.dragstart}
         @mouseup=${this.dragend}
         @mouseleave=${this.dragend}
         @mousemove=${this.drag}
-
         @touchstart=${this.dragstart}
         @touchend=${this.dragend}
-        @touchmove=${this.drag}>
-      </div>
+        @touchmove=${this.drag}
+      ></div>
     `;
   }
 
@@ -72,31 +76,34 @@ export default class WaveformProgress extends LitElement {
     this._percentComplete = this.percentComplete;
   }
 
-  private drag(e: MouseEvent) {
+  private drag(e: MouseEvent): void {
     /* istanbul ignore if */
-    if (!this._userIsInteracting) { return; }
+    if (!this._userIsInteracting) {
+      return;
+    }
     this.updatePercentComplete(e);
   }
 
-  private dragstart(e: MouseEvent) {
+  private dragstart(e: MouseEvent): void {
     this._userIsInteracting = true;
     this.updatePercentComplete(e);
   }
 
-  private dragend(e: MouseEvent) {
+  private dragend(): void {
     this._userIsInteracting = false;
   }
 
-  private updatePercentComplete(e: MouseEvent) {
-    this._percentComplete = this.offsetXToPercent(e.offsetX);
-    this.dispatchValueChangeEvent()
+  private updatePercentComplete(e: MouseEvent): void {
+    if (!this.container) { return; }
+    const containerOffset: number = this.container.offsetLeft;
+    const offsetX: number = e.pageX - containerOffset;
+    this._percentComplete = this.offsetXToPercent(offsetX);
+    this.dispatchValueChangeEvent();
   }
 
-  private dispatchValueChangeEvent() {
+  private dispatchValueChangeEvent(): void {
     const event = new CustomEvent('valuechange', {
       detail: { value: this._percentComplete },
-      bubbles: true,
-      composed: true,
     });
     this.dispatchEvent(event);
   }
@@ -105,17 +112,24 @@ export default class WaveformProgress extends LitElement {
     return this.shadowRoot && this.shadowRoot.getElementById('dragcover');
   }
 
+  private get container(): HTMLElement | null {
+    return this.shadowRoot && this.shadowRoot.querySelector('.container');
+  }
+
   private offsetXToPercent(offsetX: number): number {
     /* istanbul ignore if */
-    if (this.dragcover === null) { return 0; }
+    if (this.dragcover === null) {
+      return 0;
+    }
     const width: number = this.dragcover.clientWidth;
-    const percentComplete: number = offsetX / width * 100;
+    const percentComplete: number = (offsetX / width) * 100;
     return percentComplete;
   }
 
-  static get styles() {
+  static get styles(): CSSResult {
     const fillColorCss = css`var(--fillColor, #3272b6)`;
     const zoneOfSilenceColorCss = css`var(--zoneOfSilenceColor, #f6e652)`;
+    const waveformLeftRightMarginCss = css`var(--waveformLeftRightMarginSize, 10px)`;
 
     return css`
       :host {
@@ -126,14 +140,16 @@ export default class WaveformProgress extends LitElement {
         width: 100%;
         height: 100%;
         position: absolute;
+        touch-action: none;
       }
 
       .container {
         display: block;
         position: relative;
         background-color: white;
-        width: 100%;
         height: 100%;
+        margin-left: ${waveformLeftRightMarginCss};
+        margin-right: ${waveformLeftRightMarginCss};
       }
 
       .waveform-image {
@@ -146,7 +162,13 @@ export default class WaveformProgress extends LitElement {
         position: absolute;
         top: 0;
         bottom: 0;
-        background: linear-gradient(#000, #000 47%, ${zoneOfSilenceColorCss} 50%, #000 53%, #000 100%);
+        background: linear-gradient(
+          #000,
+          #000 47%,
+          ${zoneOfSilenceColorCss} 50%,
+          #000 53%,
+          #000 100%
+        );
       }
 
       #fill {
