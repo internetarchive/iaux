8e565dc1ac78eb633a9a91fc3942fa15c5ff4dba
Isa Herico-Velasco <isaherico@gmail.com>
2020-01-21 16:03:15 -0800

    new utils for enhanced music player's view model maker

--- packages/ia-components/sandbox/theatres/with-youtube-spotify/utils/utils.js
+++ packages/ia-components/sandbox/theatres/with-youtube-spotify/utils/utils.js
@@ -15,3 +15,24 @@ export const isValidAudioFile = (trackName = '') => !!trackName.match(/(mp3|ogg|
  * @returns { boolean }
  */
 export const isValidImageFile = (imageName = '') => !!imageName.match(/(png|jpg|jpeg)$/gi);
+
+/**
+ * isOlderDerivedMP3
+ * Older derived MP3s can still exist, and this causes double
+ * checks string for valid extension
+ *
+ * @param {string } trackName
+ * @returns { boolean }
+ */
+export const isOlderDerivedMP3 = (trackName = '') => !!trackName.match(/_vbr.mp3$/g);
+
+/**
+ * isValidSegmentFile
+ * Segment files are what IA uses to break down longer audio files to smaller ones
+ * They are created by IA's internal CD & LP ripping software
+ * checks string for valid extension
+ *
+ * @param {string } fileName
+ * @returns { boolean }
+ */
+export const isValidSegmentFile = (fileName = '') => !!fileName.match(/_segments.(json|xml)$/gi);
