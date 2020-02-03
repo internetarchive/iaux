887484ed4107f24030bd573076b56a3255b7a33e
Shane Riley <shane@shaneriley.com>
2020-02-03 11:41:13 -0500

    Creates language select component (#379)
    * Creates language select component
    
    Closes #378
    
    * Reduces vertical padding in dropdown trigger
    
    * Updates language select to use existing data structure
    
    * Prevents wrapping of language text

---
+++ packages/ia-components/sandbox/language-select/index.stories.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { languageConfig } from '../languages/Languages';
+import LanguageSelect from './language-select';
+
+const containerStyle = {
+  padding: '20px 0',
+  textAlign: 'center',
+  background: '#333',
+};
+
+const onSelect = (value) => {
+  const language = languageConfig[value];
+  console.log(`Selected language: ${language.inLocal} (${value})`);
+};
+
+storiesOf('Sandbox', module)
+  .addParameters({ jest: ['language-select.test.js'] })
+  .addWithJSX('Language Select', () => {
+    return (<div style={containerStyle}>
+      <h3 style={{ color: '#fff' }}>Language dropdown</h3>
+      <LanguageSelect selectedLanguage='en' languages={languageConfig} onSelect={onSelect} />
+    </div>);
+  })
+
