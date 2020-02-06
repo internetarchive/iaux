0d7edf505b664ec9016a9752b4fbbdf89be2c98f
Isa Herico-Velasco <isaherico@gmail.com>
2020-02-06 02:39:58 -0800

    added 78rpm to track display unit tests

--- packages/ia-components/sandbox/track-lists/lp-track-name.test.js
+++ packages/ia-components/sandbox/track-lists/lp-track-name.test.js
@@ -2,33 +2,33 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
 
-import cdTracks from './test-utils/acdc-track';
+import lpTrack from './test-utils/album_recordings-track';
 import OneTrack from './track';
 
-const { bestOfCompilation } = cdTracks;
+const { regularTrack, noTrackName } = lpTrack;
 
-describe('ACDC item track name display', () => {
+describe('ALBUM_RECORDINGS item track name display', () => {
   const component = TestRenderer.create(
     <OneTrack
       onSelected={() => {}}
       displayTrackNumber={false}
       selected={false}
-      thisTrack={bestOfCompilation}
+      thisTrack={regularTrack}
     />
   );
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 
-  const { track, length } = bestOfCompilation;
+  const { trackNumber, formattedLength } = regularTrack;
 
   describe('has basic info', () => {
-    const [trackNumber, trackTitle, trackLength] = tree.children;
+    const [trNum, trackTitle, trackLength] = tree.children;
 
     test('has track number', () => {
-      expect(trackNumber.children.join('')).toEqual(track);
+      expect(parseInt(trNum.children.join(''), 10)).toEqual(trackNumber);
     });
     test('has track length', () => {
-      expect(trackLength.children.join('')).toEqual(length);
+      expect(trackLength.children.join('')).toEqual(formattedLength);
     });
     test('has track title', () => {
       expect(trackTitle.props.className).toEqual('track-title');
@@ -39,31 +39,32 @@ describe('ACDC item track name display', () => {
   // an artist's name in the title signals that they are the album artist
   describe('When to show artist name[s]', () => {
     test('Do not show artist name if their name is in the title', () => {
-      const { album, title, creator } = bestOfCompilation;
+      const { album, title, creator } = regularTrack;
       const noArtist1 = TestRenderer.create(
         <OneTrack
           onSelected={() => {}}
           displayTrackNumber={false}
           selected={false}
-          thisTrack={bestOfCompilation}
-          albumName={creator}
+          thisTrack={regularTrack}
+          albumName={`${creator} has an amazing album`}
         />
       ).toJSON();
       const [trackName, trackTitle, trackLength] = noArtist1.children;
       const [actualTrackTitle, delimiter, artistElement] = trackTitle.children;
+
       expect(actualTrackTitle).toEqual(title);
       expect(delimiter).toEqual(undefined);
       expect(artistElement).toEqual(undefined);
     });
 
     test('Do not show artist if they exactly match the album artist', () => {
-      const { album, title, creator } = bestOfCompilation;
+      const { album, title, creator } = regularTrack;
       const noArtist2 = TestRenderer.create(
         <OneTrack
           onSelected={() => {}}
           displayTrackNumber={false}
           selected
-          thisTrack={bestOfCompilation}
+          thisTrack={regularTrack}
           albumCreator={creator}
         />
       ).toJSON();
@@ -73,6 +74,25 @@ describe('ACDC item track name display', () => {
       expect(actualTrackTitle).toEqual(title);
       expect(delimiter).toEqual(undefined);
       expect(artistElement).toEqual(undefined);
+    });
+  });
+
+  describe('Knows how to display track name', () => {
+    test('uses file name if no title', () => {
+      const { name, title, creator } = noTrackName;
+      const noArtist2 = TestRenderer.create(
+        <OneTrack
+          onSelected={() => {}}
+          displayTrackNumber={false}
+          selected
+          thisTrack={noTrackName}
+          albumCreator={creator}
+        />
+      ).toJSON();
+      const [trackNumber, trackTitle, trackLength] = noArtist2.children;
+      const [actualTrackTitle, delimiter, artistElement] = trackTitle.children;
+
+      expect(name).toContain(actualTrackTitle);
     });
   });
 });
