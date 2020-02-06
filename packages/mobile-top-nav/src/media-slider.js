dcb2ecab0490feb3486427912cc285cef4cd4989
Shane Riley <shane@shaneriley.com>
2020-02-06 16:27:41 -0500

    Moves remainder of CSS, moves demo index to root

--- packages/mobile-top-nav/src/media-slider.js
+++ packages/mobile-top-nav/src/media-slider.js
@@ -1,4 +1,5 @@
-import { LitElement, html, css } from 'lit-element';
+import { LitElement, html } from 'lit-element';
+import mediaSliderCss from './css/media-slider';
 
 class MediaSlider extends LitElement {
   static get properties() {
@@ -32,54 +33,7 @@ class MediaSlider extends LitElement {
   }
 
   static get styles() {
-    return css`
-      .information-menu {
-        position: relative;
-        padding: 0;
-        background: var(--grey20);
-        width: 100%;
-        height: 100%;
-        overflow: hidden;
-        position: relative;
-        font-size: inherit;
-      }
-
-      .menu-slider-offscreen {
-        transform: translate(-100%, -100%);
-      }
-
-      /* Secondary menu */
-      @keyframes menu-enter {
-        0% {
-          transform: translate(100%, -84%);
-        }
-        100% {
-          transform: translate(8%, -84%);
-        }
-      }
-      @keyframes menu-exit {
-        0% {
-          transform: translate(8%, -84%);
-        }
-        100% {
-          transform: translate(100%, -84%);
-        }
-      }
-      .menu-enter {
-        animation: menu-enter 0.5s forwards;
-      }
-      .menu-exit {
-        animation: menu-exit 0.5s forwards;
-      }
-
-      .info-box {
-        padding: 0 1%;
-      }
-
-      .info-box-1 {
-        font-weight: bold;
-      }
-    `;
+    return mediaSliderCss();
   }
 }
 
