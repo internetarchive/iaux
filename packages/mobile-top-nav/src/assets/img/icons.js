bb8582d9709a9b2369261702b55e5190e0298b68
Shane Riley <shane@shaneriley.com>
2020-02-11 12:42:33 -0500

    Cleans up icon rendering in media menu

---
+++ packages/mobile-top-nav/src/assets/img/icons.js
@@ -0,0 +1,17 @@
+import web from './icon_web';
+import texts from './icon_texts';
+import video from './icon_video';
+import audio from './icon_audio';
+import software from './icon_software';
+import images from './icon_images';
+const more = () => '...';
+
+export {
+  web,
+  texts,
+  video,
+  audio,
+  software,
+  images,
+  more,
+};
