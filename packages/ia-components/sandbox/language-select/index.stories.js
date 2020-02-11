f36190866dc25659ae8d2aa6e5c6194df27b3244
Isa Herico-Velasco <isaherico@gmail.com>
2020-02-11 13:35:58 -0800

    update languageConfig imports

--- packages/ia-components/sandbox/language-select/index.stories.js
+++ packages/ia-components/sandbox/language-select/index.stories.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import LanguageSelect from './language-select';
-
-const languageConfig = { // Note the flags are dragged out of the Mac Emoji and Symbol viewer
-  en: { inEnglish: 'English', inLocal: 'English', flag: '🇬🇧' },
-  fr: { inEnglish: 'French', inLocal: 'Française', flag: '🇫🇷' },
-  de: { inEnglish: 'German', inLocal: 'Deutsche ', flag: '🇩🇪' },
-  es: { inEnglish: 'Spanish', inLocal: 'Española', flag: '🇪🇸' },
-  hi: { inEnglish: 'Hindi', inLocal: 'हिंदी', flag: '🇮🇳' },
-  id: { inEnglish: 'Indonesian', inLocal: 'Bahasa', flag: '🇮🇩' },
-  it: { inEnglish: 'Italian', inLocal: 'Italiana', flag: '🇮🇹' },
-  ja: { inEnglish: 'Japanese', inLocal: '日本語', flag: '🇯🇵' },
-  mr: { inEnglish: 'Marathi', inLocal: 'मराठी', flag: '🇮🇳' },
-  my: { inEnglish: 'Myanmar', inLocal: 'မြန်မာ', flag: '🇲🇲' },
-  pt: { inEnglish: 'Portugese', inLocal: 'Portuguesa', flag: '🇵🇹' },
-};
+import languageConfig from './languageConfig';
 
 const containerStyle = {
   padding: '20px 0',
@@ -29,10 +16,12 @@ const onSelect = (value) => {
 
 storiesOf('Sandbox', module)
   .addParameters({ jest: ['language-select.test.js'] })
-  .addWithJSX('Language Select', () => {
-    return (<div style={containerStyle}>
+  .addWithJSX('Language Select', () => (
+    <div style={containerStyle}>
       <h3 style={{ color: '#fff' }}>Language dropdown</h3>
-      <LanguageSelect selectedLanguage='en' languages={languageConfig} onSelect={onSelect} />
-    </div>);
-  })
-
+      <LanguageSelect selectedLanguage="en" languages={languageConfig} onSelect={onSelect} />
+    </div>
+  ))(<div style={containerStyle}>
+    <h3 style={{ color: '#fff' }}>Language dropdown</h3>
+    <LanguageSelect selectedLanguage="en" languages={languageConfig} onSelect={onSelect} />
+     </div>);
