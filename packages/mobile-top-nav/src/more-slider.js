c2daa6e3b83282452436978f7f44a28fcf19b8fc
Shane Riley <shane@shaneriley.com>
2020-02-12 10:55:52 -0500

    Adds remaining more slider links

--- packages/mobile-top-nav/src/more-slider.js
+++ packages/mobile-top-nav/src/more-slider.js
@@ -10,6 +10,13 @@ class MoreSlider extends LitElement {
     return [
       { label: 'Donate', url: '/donate/' },
       { label: 'About', url: '/about/' },
+      { label: 'Contact', url: '/about/contact.php' },
+      { label: 'Blog', url: 'https://blog.archive.org/' },
+      { label: 'Projects', url: '/projects/' },
+      { label: 'Help', url: '/about/faqs.php' },
+      { label: 'Jobs', url: '/about/jobs.php' },
+      { label: 'Volunteer', url: '/about/volunteerpositions.php' },
+      { label: 'People', url: '/about/bios.php' },
     ];
   }
 
