3c4da79336a60d7635fd68e4a20022911c06ae8e
Shane Riley <shane@shaneriley.com>
2020-02-12 06:48:05 -0500

    Adds Wayback search, ability to use custom component for slider contents

---
+++ packages/mobile-top-nav/src/assets/img/icon_ellipses.js
@@ -0,0 +1,18 @@
+import { html } from 'lit-element';
+
+export default (fillColor) => (
+  html`
+    <svg width="24px" height="6px" viewBox="0 0 24 6" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <title>ellipses</title>
+      <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g transform="translate(-10.000000, -323.000000)" fill="${fillColor}">
+          <g transform="translate(10.000000, 323.000000)">
+            <circle cx="2.76923077" cy="2.76923077" r="2.76923077"></circle>
+            <circle cx="12" cy="2.76923077" r="2.76923077"></circle>
+            <circle cx="21.2307692" cy="2.76923077" r="2.76923077"></circle>
+          </g>
+        </g>
+      </g>
+    </svg>
+  `
+);
--- packages/mobile-top-nav/src/media-slider.js
+++ packages/mobile-top-nav/src/media-slider.js
@@ -1,6 +1,79 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, html, css } from 'lit-element';
 import mediaSliderCss from './css/media-slider';
 import menus from './data/menus';
+
+class WaybackSearch extends LitElement {
+  static get styles() {
+    return css`
+      form {
+        padding: 0 5rem 0 2rem;
+      }
+
+      p {
+        font-weight: 200;
+      }
+
+      a {
+        font-weight: 500;
+        text-decoration: none;
+        color: var(--activeColor);
+      }
+
+      fieldset {
+        max-width: 600px;
+        padding: .7rem 2rem;
+        margin: 1.5rem auto;
+        text-align: center;
+        border: none;
+        border-radius: 7px;
+        background-color: #fcf5e6;
+        box-shadow: 3px 3px 0 0 #c3ad97;
+      }
+
+      label {
+        display: none;
+      }
+
+      img {
+        width: 215px;
+        height: 60px;
+        max-width: 100%;
+        vertical-align: middle;
+      }
+
+      input {
+        display: block;
+        width: 100%;
+        height: 3rem;
+        padding: .5rem 1rem .5rem 2.5rem;
+        font-size: 1.2rem;
+        line-height: 1.5;
+        box-sizing: border-box;
+        border-radius: 2rem;
+        background: #eee;
+      }
+    `;
+  }
+
+  redirectToWayback(e) {
+    e.preventDefault();
+    const url = e.target.querySelector('#url').value;
+    window.location = `https://web.archive.org/web/*/${url}`;
+  }
+
+  render() {
+    return html`
+      <form action="" method="post" @submit=${this.redirectToWayback}>
+        <p>Search the history of over 411 billion <a data-event-click-tracking="TopNav|WaybackMachineStatsLink" href="https://blog.archive.org/2016/10/23/defining-web-pages-web-sites-and-web-captures/">web pages</a> on the Internet.</p>
+        <fieldset>
+          <a data-event-click-tracking="TopNav|WaybackMachineLogoLink" href="https://archive.org/web/"><img src="https://archive.org/images/WaybackLogoSmall.png" alt="Wayback Machine"></a>
+          <label for="url">Search the Wayback Machine</label>
+          <input type="text" name="url" id="url" placeholder="enter URL or keywords" />
+        </fieldset>
+      </form>
+    `;
+  }
+}
 
 class MediaSlider extends LitElement {
   constructor() {
@@ -8,7 +81,12 @@ class MediaSlider extends LitElement {
     this.mediaSliderOpen = false;
     this.mediaSliderAnimate = false;
     this.selectedMenuOption = 'texts';
+
+    // Begin properties not monitored by LitElement
     this.links = menus[this.selectedMenuOption];
+    this.templates = {
+      web: () => html`<wayback-search></wayback-search>`
+    };
   }
 
   static get properties() {
@@ -35,6 +113,29 @@ class MediaSlider extends LitElement {
     return this.links[category].map((link) => html`<li><a href="${link.url}">${link.title}</a></li>`);
   }
 
+  renderSubnav() {
+    const template = this.templates[this.selectedMenuOption];
+
+    if (template) {
+      return template();
+    }
+
+    return html`
+      <h3>${this.links.heading}</h3>
+      <div class="icon-links">
+        ${this.renderIconLinks()}
+      </div>
+      <h4>Featured</h4>
+      <ul>
+        ${this.renderLinks('featuredLinks')}
+      </ul>
+      <h4>Top</h4>
+      <ul>
+        ${this.renderLinks('links')}
+      </ul>
+    `;
+  }
+
   render() {
     let sliderDetailsClass = 'menu-slider-offscreen';
 
@@ -49,18 +150,7 @@ class MediaSlider extends LitElement {
     return html`
       <div class="information-menu ${sliderDetailsClass}">
         <div class="info-box">
-          <h3>${this.links.heading}</h3>
-          <div class="icon-links">
-            ${this.renderIconLinks()}
-          </div>
-          <h4>Featured</h4>
-          <ul>
-            ${this.renderLinks('featuredLinks')}
-          </ul>
-          <h4>Top</h4>
-          <ul>
-            ${this.renderLinks('links')}
-          </ul>
+          ${this.renderSubnav()}
         </div>
       </div>
     `;
@@ -71,4 +161,5 @@ class MediaSlider extends LitElement {
   }
 }
 
+customElements.define('wayback-search', WaybackSearch);
 customElements.define('media-slider', MediaSlider);
