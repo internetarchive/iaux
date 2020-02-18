0cd7b908905c6d680019c2f16f43c58f7810c819
Shane Riley <shane@shaneriley.com>
2020-02-18 11:35:10 -0500

    Adds rounded linecaps to hamburger

--- packages/mobile-top-nav/src/assets/img/hamburger.js
+++ packages/mobile-top-nav/src/assets/img/hamburger.js
@@ -16,10 +16,14 @@ class HamBurger extends Icon {
   render() {
     return html`
       <svg alt="Main menu" width="24px" height="19px" viewBox="0 0 24 19" xmlns="http://www.w3.org/2000/svg">
-        <g stroke-width="3" class="${this.active ? 'active' : ''}">
-          <line x1="0" y1="1.5" x2="20" y2="1.5"></line>
-          <line x1="0" y1="9.5" x2="20" y2="9.5"></line>
-          <line x1="0" y1="17.5" x2="20" y2="17.5"></line>
+        <g class="${this.active ? 'active' : ''}" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round">
+          <g transform="translate(-9.000000, -11.000000)" stroke="#999999" stroke-width="3">
+              <g transform="translate(11.000000, 12.000000)">
+                <line x1="0" y1="0.5" x2="20" y2="0.5"></line>
+                <line x1="0" y1="8.5" x2="20" y2="8.5"></line>
+                <line x1="0" y1="16.5" x2="20" y2="16.5"></line>
+              </g>
+          </g>
         </g>
       </svg>
     `;
