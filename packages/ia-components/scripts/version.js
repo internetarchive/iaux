01e5411623c9aab9ff3feef5ea20bb4b6e2b8c90
Shane Riley <shane@shaneriley.com>
2020-03-18 06:21:45 -0400

    Changes to match institutional coding style

--- packages/ia-components/scripts/version.js
+++ packages/ia-components/scripts/version.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
 const { execSync } = require('child_process');
 const { version: NEW_VERSION } = require('../package.json');
+const writeFence = '<!-- Write here... -->';
 
 async function main() {
   // Update the changelog
-  const old_changelog = fs.readFileSync('CHANGELOG.md').toString();
-  const new_changelog = `# ${NEW_VERSION}\n<!-- Write here... -->\n\n` + old_changelog;
-  fs.writeFileSync('CHANGELOG.md', new_changelog);
-  while (fs.readFileSync('CHANGELOG.md').toString().includes('<!-- Write here... -->')) {
+  const oldChangelog = fs.readFileSync('CHANGELOG.md').toString();
+  const newChangelog = `# ${NEW_VERSION}\n${writeFence}\n\n` + oldChangelog;
+  fs.writeFileSync('CHANGELOG.md', newChangelog);
+  while (fs.readFileSync('CHANGELOG.md').toString().includes(writeFence)) {
     console.log('Changelog contains dummy text. Update the changelog, and press ENTER continue.');
-    await new Promise(res => process.stdin.once("data", res));
+    await new Promise(res => process.stdin.once('data', res));
   }
   execSync('git add CHANGELOG.md');
 }
