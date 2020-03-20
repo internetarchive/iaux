15d5be9e680bd82d66f92dbf763c542aa8b98121
Jason Buckner <jason@jasonbuckner.com>
2020-03-19 17:15:11 -0700

    Start work on loading indicator component

---
+++ packages/ia-loading-indicator/src/ia-loading-indicator.ts
@@ -0,0 +1,175 @@
+import { LitElement, html, css, property, CSSResult, TemplateResult } from 'lit-element';
+
+enum IALoadingIndicatorImageMode {
+  loading,
+  complete,
+}
+
+// eslint-disable-next-line import/prefer-default-export
+class IALoadingIndicator extends LitElement {
+  // eslint-disable-next-line max-len
+  @property({ type: Number }) mode: IALoadingIndicatorImageMode =
+    IALoadingIndicatorImageMode.loading;
+
+  @property({ type: String }) checkmarkColor = '#31A481';
+
+  @property({ type: String }) completedRingColor = '#31A481';
+
+  @property({ type: String }) loadingRingColor = '#333333';
+
+  @property({ type: String }) loadingDotColor = '#333333';
+
+  static get styles(): CSSResult {
+    const checkmarkColorCss = css`var(--checkmarkColor, #31A481)`;
+    const completedRingColorCss = css`var(--completedRingColor, #31A481)`;
+    const loadingRingColorCss = css`var(--loadingRingColor, #333333)`;
+    const loadingDotColorCss = css`var(--loadingDotColor, #333333)`;
+
+    return css`
+      #completed-ring {
+        fill: ${completedRingColorCss};
+      }
+
+      #check {
+        fill: ${checkmarkColorCss};
+      }
+
+      #progress-ring {
+        fill: ${loadingRingColorCss};
+      }
+
+      #progress-dots {
+        fill: ${loadingDotColorCss};
+      }
+
+      .processing-image svg {
+        margin: auto;
+        width: 75px;
+        height: 75px;
+      }
+
+      .progress-indicator {
+        opacity: 0;
+        transition: opacity 0.25s ease-out;
+      }
+
+      .loading .progress-indicator {
+        opacity: 1;
+      }
+
+      .loaded-indicator {
+        opacity: 1;
+        transition: opacity 0.25s ease-out;
+      }
+
+      .loading .loaded-indicator {
+        opacity: 0;
+      }
+
+      .image {
+        border: 1px solid red;
+        display: inline-block;
+      }
+
+      .loading #progress-ring {
+        animation: rotate 1.3s infinite linear;
+        transform-origin: 50px 50px;
+        transform-box: fill-box;
+      }
+
+      .loading #left-dot {
+        opacity: 0;
+        animation: dot 1.3s infinite;
+        animation-delay: 0.2s;
+      }
+
+      .loading #middle-dot {
+        opacity: 0;
+        animation: dot 1.3s infinite;
+        animation-delay: 0.4s;
+      }
+
+      .loading #right-dot {
+        opacity: 0;
+        animation: dot 1.3s infinite;
+        animation-delay: 0.6s;
+      }
+
+      @keyframes rotate {
+        0% {
+          transform: rotate(-360deg);
+        }
+      }
+
+      @keyframes dot {
+        0% {
+          opacity: 0;
+        }
+        25% {
+          opacity: 1;
+        }
+        100% {
+          opacity: 0;
+        }
+      }
+    `;
+  }
+
+  render(): TemplateResult {
+    /* eslint-disable */
+    return html`
+      <div
+        class="processing-image ${this.mode === IALoadingIndicatorImageMode.loading
+          ? 'loading'
+          : ''}"
+      >
+        <svg
+          viewBox="0 0 120 120"
+          preserveAspectRatio="none"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+        >
+          <g
+            id="icons/check-ring---squared"
+            stroke="none"
+            stroke-width="1"
+            fill="none"
+            fill-rule="evenodd"
+          >
+            <path
+              id="completed-ring"
+              class="loaded-indicator"
+              d="M60,10 C70.5816709,10 80.3955961,13.2871104 88.4763646,18.8959201 L78.3502633,29.0214223 C72.9767592,25.8315427 66.7022695,24 60,24 C40.117749,24 24,40.117749 24,60 C24,79.882251 40.117749,96 60,96 C79.882251,96 96,79.882251 96,60 L95.995,59.46 L108.327675,47.128668 C109.350926,50.9806166 109.925886,55.015198 109.993301,59.1731586 L110,60 C110,87.6142375 87.6142375,110 60,110 C32.3857625,110 10,87.6142375 10,60 C10,32.3857625 32.3857625,10 60,10 Z"
+            ></path>
+            <polygon
+              id="check"
+              class="loaded-indicator"
+              transform="translate(75.000000, 41.500000) rotate(44.000000) translate(-75.000000, -41.500000) "
+              points="96 85 54 85 54 65 76 64.999 76 -2 96 -2"
+            ></polygon>
+            <path
+              id="progress-ring"
+              class="progress-indicator"
+              d="M60,10 C69.8019971,10 78.9452178,12.8205573 86.6623125,17.6943223 L76.4086287,27.9484118 C71.4880919,25.4243078 65.9103784,24 60,24 C40.117749,24 24,40.117749 24,60 C24,79.882251 40.117749,96 60,96 C79.882251,96 96,79.882251 96,60 C96,53.3014663 94.1704984,47.0302355 90.9839104,41.6587228 L101.110332,31.5326452 C106.715332,39.6116982 110,49.4222615 110,60 C110,87.6142375 87.6142375,110 60,110 C32.3857625,110 10,87.6142375 10,60 C10,32.3857625 32.3857625,10 60,10 Z"
+            ></path>
+            <g
+              id="progress-dots"
+              class="progress-indicator"
+              transform="translate(40.000000, 55.000000)"
+            >
+              <circle id="left-dot" cx="5" cy="5" r="5"></circle>
+              <circle id="middle-dot" cx="20" cy="5" r="5"></circle>
+              <circle id="right-dot" cx="35" cy="5" r="5"></circle>
+            </g>
+          </g>
+        </svg>
+      </div>
+    `;
+    /* eslint-enable */
+  }
+}
+
+customElements.define('ia-loading-indicator', IALoadingIndicator);
+
+export { IALoadingIndicator, IALoadingIndicatorImageMode };
