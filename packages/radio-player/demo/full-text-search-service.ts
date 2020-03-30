37e9b89bc5f4424ef8a37e987443e021450e7d6e
Jason Buckner <jason@jasonbuckner.com>
2020-03-30 16:12:29 -0700

    Add more JSDocs

--- packages/radio-player/demo/full-text-search-service.ts
+++ packages/radio-player/demo/full-text-search-service.ts
@@ -1,6 +1,10 @@
 import { FullTextSearchServiceInterface } from '../src/search-handler/search-backends/full-text-search-backend/full-text-search-service-interface';
 import { FullTextSearchResponse } from '../src/search-handler/search-backends/full-text-search-backend/full-text-search-response';
 
+/**
+ * An example FullTextSearchService that fetches data from the backend and
+ * returns a Promise<FullTextSearch> response.
+ */
 export class FullTextSearchService implements FullTextSearchServiceInterface {
   private itemId: string;
 
@@ -14,6 +18,7 @@ export class FullTextSearchService implements FullTextSearchServiceInterface {
     this.searchServicePath = searchServicePath;
   }
 
+  /** @inheritdoc */
   async searchRequested(query: string): Promise<FullTextSearchResponse> {
     const searchUrl = `${this.baseUrl}${this.searchServicePath}`;
     const queryParams = `?q=${query}&identifier=${this.itemId}&number_of_fragments=1000&scope=all`;
