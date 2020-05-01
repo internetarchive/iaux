7f7496a9d1d382ee64182de3ab6b93e0a4b5de1b
Shane Riley <shane@shaneriley.com>
2020-05-01 12:24:08 -0400

    Sets hard pixel offset to ensure user menu offscren (#432)

--- packages/mobile-top-nav/src/user-menu.js
+++ packages/mobile-top-nav/src/user-menu.js
@@ -5,7 +5,7 @@ class UserMenu extends TrackedElement {
   static get styles() {
     return css`
       :host {
-        --topOffset: -70vh;
+        --topOffset: -1300px;
       }
       nav {
         position: absolute;
