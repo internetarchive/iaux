25482851ab11cbf5bfd393987d86cae36e859bb3
Shane Riley <shane@shaneriley.com>
2020-05-07 16:28:56 -0400

    Adds max height and overflow on link list in dropdowns

--- packages/ia-topnav/src/styles/dropdown-menu.js
+++ packages/ia-topnav/src/styles/dropdown-menu.js
@@ -45,6 +45,9 @@ export default css`
     padding: 0.4rem 0 0.7rem 0;
     margin: 0;
     list-style: none;
+    max-height: calc(100vh - 7.2rem + 1px);
+    overflow: auto;
+    box-sizing: border-box;
   }
 
   .divider {
@@ -94,6 +97,10 @@ export default css`
 
     h3 {
       display: none;
+    }
+
+    ul {
+      max-height: calc(100vh - 8.5rem + 1px);
     }
 
     .divider {
