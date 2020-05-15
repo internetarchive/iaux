0137b2c0df18253e0c409a722d625db451abffc6
Shane Riley <shane@shaneriley.com>
2020-05-15 17:23:54 -0400

    Adds location handler for searching TV captions (#442)
    * Adds location handler for searching TV captions
    
    * Updates version

--- packages/ia-topnav/test/nav-search.test.js
+++ packages/ia-topnav/test/nav-search.test.js
@@ -2,7 +2,9 @@ import {
   html,
   fixture,
   expect,
+  oneEvent,
 } from '@open-wc/testing';
+import sinon from 'sinon';
 
 import '../src/nav-search';
 
@@ -23,5 +25,25 @@ describe('<nav-search>', () => {
     });
 
     expect(result).to.be.false;
+  });
+
+  it('redirects to the TV details page when search inside is TV', async () => {
+    const query = 'bananas';
+    const submitEvent = {
+      type: 'submit',
+      preventDefault: () => {}
+    };
+    const locationHandler = sinon.fake();
+    const el = await fixture(html`<nav-search .locationHandler=${locationHandler}></nav-search>`);
+
+    el.searchIn = 'TV';
+
+    await el.updateComplete;
+
+    el.shadowRoot.querySelector('[name=query]').value = query;
+    el.search(submitEvent);
+
+    expect(locationHandler.callCount).to.equal(1);
+    expect(locationHandler.firstArg).to.contain(`/details/tv?q=${query}`);
   });
 });
