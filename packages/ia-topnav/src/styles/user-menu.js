a350d114b96a38e78b3776081cf8a2a7a9a9ceb2
Shane Riley <shane@shaneriley.com>
2020-05-19 15:29:15 -0400

    Repositions user menu when username hidden

--- packages/ia-topnav/src/styles/user-menu.js
+++ packages/ia-topnav/src/styles/user-menu.js
@@ -5,6 +5,20 @@ export default css`
     .initial,
     .closed,
     .open {
+      right: 21.3rem;
+    }
+
+    .search-hidden.initial,
+    .search-hidden.closed,
+    .search-hidden.open {
+      right: 5.8rem;
+    }
+  }
+
+  @media (min-width: 990px) {
+    .initial,
+    .closed,
+    .open {
       right: 27.5rem;
     }
 
