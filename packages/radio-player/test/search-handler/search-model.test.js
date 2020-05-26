eec0e3df4b4859163a8ae598feb4c9eb1193366a
Jason Buckner <jason@jasonbuckner.com>
2020-05-26 12:46:03 -0700

    Bump versions and add tests

--- packages/radio-player/test/search-handler/search-model.test.js
+++ packages/radio-player/test/search-handler/search-model.test.js
@@ -2,7 +2,8 @@ import {
   expect
 } from '@open-wc/testing';
 
-import { Range } from '../../lib/src/search-handler/search-models';
+import { TranscriptEntryConfig } from '@internetarchive/transcript-view';
+import { Range, SearchResult, TranscriptEntryRange } from '../../lib/src/search-handler/search-models';
 
 describe('Search Models', () => {
   describe('Range', () => {
@@ -14,6 +15,27 @@ describe('Search Models', () => {
     it('correctly calculates the length of the range is end is less than start', async () => {
       const range = new Range(7, 2);
       expect(range.length).to.equal(5);
+    });
+  });
+
+  describe('SearchResult', () => {
+    it('can be initialized properly', async () => {
+      const range = new Range(9, 12);
+      const result = new SearchResult(range, 'foo', true);
+      expect(result.range.length).to.equal(3);
+      expect(result.text).to.equal('foo');
+      expect(result.isSearchMatch).to.equal(true);
+    });
+  });
+
+  describe('TranscriptEntryRange', () => {
+    it('can be initialized properly', async () => {
+      const transcript = new TranscriptEntryConfig(1, 17, 23, 'foo bar baz', false);
+      const range = new Range(34, 44);
+      const transcriptRange = new TranscriptEntryRange(transcript, range);
+
+      expect(transcriptRange.range.length).to.equal(10);
+      expect(transcriptRange.entry.rawText).to.equal('foo bar baz');
     });
   });
 });
