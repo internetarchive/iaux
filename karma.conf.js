bd00c6d9ed8b5b9287d71612e4039ea20ccf65de
Jason Buckner <jason@jasonbuckner.com>
2020-06-18 13:39:17 -0700

    Start work on shared karma runner

---
+++ karma.conf.js
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { createDefaultConfig } = require('@open-wc/testing-karma');
+const merge = require('deepmerge');
+
+module.exports = (config) => {
+  config.set(
+    merge(createDefaultConfig(config), {
+      files: [
+        // runs all files ending with .test in the test folder,
+        // can be overwritten by passing a --grep flag. examples:
+        //
+        // npm run test -- --grep test/foo/bar.test.js
+        // npm run test -- --grep test/bar/*
+        {
+          pattern: config.grep ? config.grep : 'packages/**/*/test/**/*.test.js',
+          type: 'module',
+        },
+      ],
+
+      esm: {
+        preserveSymlinks: true,
+        nodeResolve: true
+      },
+
+      coverageReporter: {
+        reporters: [
+          // generates ./coverage/lcov.info
+          { type: 'lcovonly', subdir: '.' },
+          // generates ./coverage/coverage-final.json
+          { type: 'json', subdir: '.' },
+        ]
+      },
+    }),
+  );
+  return config;
+};
