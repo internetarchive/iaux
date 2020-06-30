d24d24a1c5e5c47cd3d3cdb0144b2d584710d011
Shane Riley <shane@shaneriley.com>
2020-06-30 16:56:33 -0400

    Adds sample menu object from Petabox

---
+++ packages/ia-topnav/src/data/petabox_signedout_menus.js
@@ -0,0 +1,509 @@
+/* eslint-disable */
+export default {
+  "audio": {
+    "heading": "Internet Archive Audio",
+    "iconLinks": [
+      {
+        "icon": "https://www-shaner4.archive.org/services/img/etree",
+        "title": "Live Music Archive",
+        "url": "https://www-shaner4.archive.org/details/etree"
+      },
+      {
+        "icon": "https://www-shaner4.archive.org/services/img/librivoxaudio",
+        "title": "Librivox Free Audio",
+        "url": "https://www-shaner4.archive.org/details/librivoxaudio"
+      }
+    ],
+    "featuredLinks": [
+      {
+        "title": "All audio",
+        "url": "https://www-shaner4.archive.org/details/audio"
+      },
+      {
+        "title": "This Just In",
+        "url": "https://www-shaner4.archive.org/search.php?query=mediatype:audio&sort=-publicdate"
+      },
+      {
+        "title": "Grateful Dead",
+        "url": "https://www-shaner4.archive.org/details/GratefulDead"
+      },
+      {
+        "title": "Netlabels",
+        "url": "https://www-shaner4.archive.org/details/netlabels"
+      },
+      {
+        "title": "Old Time Radio",
+        "url": "https://www-shaner4.archive.org/details/oldtimeradio"
+      },
+      {
+        "title": "78 RPMs and Cylinder Recordings",
+        "url": "https://www-shaner4.archive.org/details/78rpm"
+      }
+    ],
+    "links": [
+      {
+        "title": "Audio Books & Poetry",
+        "url": "https://www-shaner4.archive.org/audio_bookspoetry"
+      },
+      {
+        "title": "Community Audio",
+        "url": "https://www-shaner4.archive.org/opensource_audio"
+      },
+      {
+        "title": "Computers, Technology and Science",
+        "url": "https://www-shaner4.archive.org/audio_tech"
+      },
+      {
+        "title": "Live Music Archive",
+        "url": "https://www-shaner4.archive.org/etree"
+      },
+      {
+        "title": "Music, Arts & Culture",
+        "url": "https://www-shaner4.archive.org/audio_music"
+      },
+      {
+        "title": "Netlabels",
+        "url": "https://www-shaner4.archive.org/netlabels"
+      },
+      {
+        "title": "News & Public Affairs",
+        "url": "https://www-shaner4.archive.org/audio_news"
+      },
+      {
+        "title": "Non-English Audio",
+        "url": "https://www-shaner4.archive.org/audio_foreign"
+      },
+      {
+        "title": "Spirituality & Religion",
+        "url": "https://www-shaner4.archive.org/audio_religion"
+      },
+      {
+        "title": "Podcasts",
+        "url": "https://www-shaner4.archive.org/details/podcasts"
+      }
+    ]
+  },
+  "images": {
+    "heading": "Images",
+    "iconLinks": [
+      {
+        "icon": "https://www-shaner4.archive.org/services/img/metropolitanmuseumofart-gallery",
+        "title": "Metropolitan Museum",
+        "url": "https://www-shaner4.archive.org/details/metropolitanmuseumofart-gallery"
+      },
+      {
+        "icon": "https://www-shaner4.archive.org/services/img/brooklynmuseum",
+        "title": "Brooklyn Museum",
+        "url": "https://www-shaner4.archive.org/details/brooklynmuseum"
+      }
+    ],
+    "featuredLinks": [
+      {
+        "title": "All images",
+        "url": "https://www-shaner4.archive.org/details/image"
+      },
+      {
+        "title": "This Just In",
+        "url": "https://www-shaner4.archive.org/search.php?query=mediatype:image&sort=-publicdate"
+      },
+      {
+        "title": "Flickr Commons",
+        "url": "https://www-shaner4.archive.org/details/flickrcommons"
+      },
+      {
+        "title": "Occupy Wall Street Flickr",
+        "url": "https://www-shaner4.archive.org/details/flickr-ows"
+      },
+      {
+        "title": "Cover Art",
+        "url": "https://www-shaner4.archive.org/details/coverartarchive"
+      },
+      {
+        "title": "USGS Maps",
+        "url": "https://www-shaner4.archive.org/details/maps_usgs"
+      }
+    ],
+    "links": [
+      {
+        "title": "NASA Images",
+        "url": "https://www-shaner4.archive.org/details/nasa"
+      },
+      {
+        "title": "Solar System Collection",
+        "url": "https://www-shaner4.archive.org/details/solarsystemcollection"
+      },
+      {
+        "title": "Ames Research Center",
+        "url": "https://www-shaner4.archive.org/details/amesresearchcenterimagelibrary"
+      }
+    ]
+  },
+  "more": [
+    {
+      "label": "About",
+      "url": "https://www-shaner4.archive.org/about/"
+    },
+    {
+      "label": "Blog",
+      "url": "https://blog.archive.org/"
+    },
+    {
+      "label": "Projects",
+      "url": "https://www-shaner4.archive.org/projects/"
+    },
+    {
+      "label": "Help",
+      "url": "https://www-shaner4.archive.org/about/faqs.php"
+    },
+    {
+      "label": "Donate",
+      "url": "https://www-shaner4.archive.org/donate/"
+    },
+    {
+      "label": "Contact",
+      "url": "https://www-shaner4.archive.org/about/contact.php"
+    },
+    {
+      "label": "Jobs",
+      "url": "https://www-shaner4.archive.org/about/jobs.php"
+    },
+    {
+      "label": "Volunteer",
+      "url": "https://www-shaner4.archive.org/about/volunteerpositions.php"
+    },
+    {
+      "label": "People",
+      "url": "https://www-shaner4.archive.org/about/bios.php"
+    }
+  ],
+  "software": {
+    "heading": "Software",
+    "iconLinks": [
+      {
+        "icon": "https://www-shaner4.archive.org/services/img/internetarcade",
+        "title": "Internet Arcade",
+        "url": "https://www-shaner4.archive.org/details/internetarcade"
+      },
+      {
+        "icon": "https://www-shaner4.archive.org/services/img/consolelivingroom",
+        "title": "Console Living Room",
+        "url": "https://www-shaner4.archive.org/details/consolelivingroom"
+      }
+    ],
+    "featuredLinks": [
+      {
+        "title": "All software",
+        "url": "https://www-shaner4.archive.org/details/software"
+      },
+      {
+        "title": "This Just In",
+        "url": "https://www-shaner4.archive.org/search.php?query=mediatype:software&sort=-publicdate"
+      },
+      {
+        "title": "Old School Emulation",
+        "url": "https://www-shaner4.archive.org/details/tosec"
+      },
+      {
+        "title": "MS-DOS Games",
+        "url": "https://www-shaner4.archive.org/details/softwarelibrary_msdos_games"
+      },
+      {
+        "title": "Historical Software",
+        "url": "https://www-shaner4.archive.org/details/historicalsoftware"
+      },
+      {
+        "title": "Classic PC Games",
+        "url": "https://www-shaner4.archive.org/details/classicpcgames"
+      },
+      {
+        "title": "Software Library",
+        "url": "https://www-shaner4.archive.org/details/softwarelibrary"
+      }
+    ],
+    "links": [
+      {
+        "title": "Kodi Archive and Support File",
+        "url": "https://www-shaner4.archive.org/details/kodi_archive"
+      },
+      {
+        "title": "Community Software",
+        "url": "https://www-shaner4.archive.org/details/open_source_software"
+      },
+      {
+        "title": "Vintage Software",
+        "url": "https://www-shaner4.archive.org/details/vintagesoftware"
+      },
+      {
+        "title": "APK",
+        "url": "https://www-shaner4.archive.org/details/apkarchive"
+      },
+      {
+        "title": "MS-DOS",
+        "url": "https://www-shaner4.archive.org/details/softwarelibrary_msdos"
+      },
+      {
+        "title": "CD-ROM Software",
+        "url": "https://www-shaner4.archive.org/details/cd-roms"
+      },
+      {
+        "title": "CD-ROM Software Library",
+        "url": "https://www-shaner4.archive.org/details/cdromsoftware"
+      },
+      {
+        "title": "Software Sites",
+        "url": "https://www-shaner4.archive.org/details/softwaresites"
+      },
+      {
+        "title": "Tucows Software Library",
+        "url": "https://www-shaner4.archive.org/details/tucows"
+      },
+      {
+        "title": "Shareware CD-ROMs",
+        "url": "https://www-shaner4.archive.org/details/cdbbsarchive"
+      },
+      {
+        "title": "Software Capsules Compilation",
+        "url": "https://www-shaner4.archive.org/details/softwarecapsules"
+      },
+      {
+        "title": "CD-ROM Images",
+        "url": "https://www-shaner4.archive.org/details/cdromimages"
+      },
+      {
+        "title": "ZX Spectrum",
+        "url": "https://www-shaner4.archive.org/details/softwarelibrary_zx_spectrum"
+      },
+      {
+        "title": "DOOM Level CD",
+        "url": "https://www-shaner4.archive.org/details/doom-cds"
+      }
+    ]
+  },
+  "texts": {
+    "heading": "Books",
+    "iconLinks": [
+      {
+        "title": "Books to Borrow",
+        "icon": "https://www-shaner4.archive.org/images/book-lend.png",
+        "url": "https://www-shaner4.archive.org/details/inlibrary"
+      },
+      {
+        "title": "Open Library",
+        "icon": "https://www-shaner4.archive.org/images/widgetOL.png",
+        "url": "https://openlibrary.org/"
+      }
+    ],
+    "featuredLinks": [
+      {
+        "title": "All Books",
+        "url": "https://www-shaner4.archive.org/details/books"
+      },
+      {
+        "title": "All Texts",
+        "url": "https://www-shaner4.archive.org/details/texts"
+      },
+      {
+        "title": "This Just In",
+        "url": "https://www-shaner4.archive.org/search.php?query=mediatype:texts&sort=-publicdate"
+      },
+      {
+        "title": "Smithsonian Libraries",
+        "url": "https://www-shaner4.archive.org/details/smithsonian"
+      },
+      {
+        "title": "FEDLINK (US)",
+        "url": "https://www-shaner4.archive.org/details/fedlink"
+      },
+      {
+        "title": "Genealogy",
+        "url": "https://www-shaner4.archive.org/details/genealogy"
+      },
+      {
+        "title": "Lincoln Collection",
+        "url": "https://www-shaner4.archive.org/details/lincolncollection"
+      }
+    ],
+    "links": [
+      {
+        "title": "American Libraries",
+        "url": "https://www-shaner4.archive.org/americana"
+      },
+      {
+        "title": "Canadian Libraries",
+        "url": "https://www-shaner4.archive.org/toronto"
+      },
+      {
+        "title": "Universal Library",
+        "url": "https://www-shaner4.archive.org/universallibrary"
+      },
+      {
+        "title": "Community Texts",
+        "url": "https://www-shaner4.archive.org/opensource"
+      },
+      {
+        "title": "Project Gutenberg",
+        "url": "https://www-shaner4.archive.org/gutenberg"
+      },
+      {
+        "title": "Biodiversity Heritage Library",
+        "url": "https://www-shaner4.archive.org/biodiversity"
+      },
+      {
+        "title": "Children%27s Library",
+        "url": "https://www-shaner4.archive.org/iacl"
+      },
+      {
+        "title": "Books by Language",
+        "url": "https://www-shaner4.archive.org/details/booksbylanguage"
+      },
+      {
+        "title": "Additional Collections",
+        "url": "https://www-shaner4.archive.org/details/additional_collections"
+      }
+    ]
+  },
+  "web": {
+    "mobileAppsLinks": [
+      {
+        "url": "https://apps.apple.com/us/app/wayback-machine/id1201888313",
+        "title": "Wayback Machine (iOS)",
+        "external": true
+      },
+      {
+        "url": "https://play.google.com/store/apps/details?id=com.archive.waybackmachine&hl=en_US",
+        "title": "Wayback Machine (Android)",
+        "external": true
+      }
+    ],
+    "browserExtensionsLinks": [
+      {
+        "url": "https://chrome.google.com/webstore/detail/save-to-the-wayback-machi/eebpioaailbjojmdbmlpomfgijnlcemk?hl=en",
+        "title": "Chrome",
+        "external": true
+      },
+      {
+        "url": "https://addons.mozilla.org/en-US/firefox/addon/wayback-machine_new/",
+        "title": "Firefox",
+        "external": true
+      },
+      {
+        "url": "https://apps.apple.com/us/app/wayback-machine/id1472432422?mt=12",
+        "title": "Safari",
+        "external": true
+      }
+    ],
+    "archiveItLinks": [
+      {
+        "url": "https://www.archive-it.org/explore",
+        "title": "Explore the Collections"
+      },
+      {
+        "url": "https://www.archive-it.org/blog/learn-more/",
+        "title": "Learn More"
+      },
+      {
+        "url": "https://www.archive-it.org/contact-us",
+        "title": "Build Collections"
+      }
+    ]
+  },
+  "video": {
+    "heading": "Video",
+    "iconLinks": [
+      {
+        "icon": "https://www-shaner4.archive.org/services/img/tv",
+        "title": "TV News",
+        "url": "https://www-shaner4.archive.org/details/tv"
+      },
+      {
+        "icon": "https://www-shaner4.archive.org/services/img/911",
+        "title": "Understanding 9/11",
+        "url": "https://www-shaner4.archive.org/details/911"
+      }
+    ],
+    "featuredLinks": [
+      {
+        "title": "All video",
+        "url": "https://www-shaner4.archive.org/details/movies"
+      },
+      {
+        "title": "This Just In",
+        "url": "https://www-shaner4.archive.org/search.php?query=mediatype:movies&sort=-publicdate"
+      },
+      {
+        "title": "Prelinger Archives",
+        "url": "https://www-shaner4.archive.org/details/prelinger"
+      },
+      {
+        "title": "Democracy Now!",
+        "url": "https://www-shaner4.archive.org/details/democracy_now_vid"
+      },
+      {
+        "title": "Occupy Wall Street",
+        "url": "https://www-shaner4.archive.org/details/occupywallstreet"
+      },
+      {
+        "title": "TV NSA Clip Library",
+        "url": "https://www-shaner4.archive.org/details/nsa"
+      }
+    ],
+    "links": [
+      {
+        "title": "Animation & Cartoons",
+        "url": "https://www-shaner4.archive.org/animationandcartoons"
+      },
+      {
+        "title": "Arts & Music",
+        "url": "https://www-shaner4.archive.org/artsandmusicvideos"
+      },
+      {
+        "title": "Computers & Technology",
+        "url": "https://www-shaner4.archive.org/computersandtechvideos"
+      },
+      {
+        "title": "Cultural & Academic Films",
+        "url": "https://www-shaner4.archive.org/culturalandacademicfilms"
+      },
+      {
+        "title": "Ephemeral Films",
+        "url": "https://www-shaner4.archive.org/ephemera"
+      },
+      {
+        "title": "Movies",
+        "url": "https://www-shaner4.archive.org/moviesandfilms"
+      },
+      {
+        "title": "News & Public Affairs",
+        "url": "https://www-shaner4.archive.org/newsandpublicaffairs"
+      },
+      {
+        "title": "Prelinger Archives",
+        "url": "https://www-shaner4.archive.org/prelinger"
+      },
+      {
+        "title": "Spirituality & Religion",
+        "url": "https://www-shaner4.archive.org/spiritualityandreligion"
+      },
+      {
+        "title": "Sports Videos",
+        "url": "https://www-shaner4.archive.org/sports"
+      },
+      {
+        "title": "Norton Media Center",
+        "url": "https://www-shaner4.archive.org/details/nmcma"
+      }
+    ]
+  },
+  "signedOut": [
+    {
+      "href": "https://www-shaner4.archive.org/account/signup",
+      "title": "Sign up for free",
+      "analyticsEvent": "AvatarMenu-Signup"
+    },
+    {
+      "href": "https://www-shaner4.archive.org/account/login",
+      "title": "Log in",
+      "analyticsEvent": "AvatarMenu-Login"
+    }
+  ]
+};
