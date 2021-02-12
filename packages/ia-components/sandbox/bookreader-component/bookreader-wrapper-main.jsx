4d3a41e2d9426740fb85307e3edb930e0094845e
Isa Herico-Velasco <isaherico@gmail.com>
2021-02-11 16:34:53 -0800

    liner notes BR - update config & make sure we are using size reducer when creating img url

--- packages/ia-components/sandbox/bookreader-component/bookreader-wrapper-main.jsx
+++ packages/ia-components/sandbox/bookreader-component/bookreader-wrapper-main.jsx
@@ -30,6 +30,7 @@ export default class BookReaderWrapper extends Component {
   constructor(props) {
     super(props);
     this.BookReaderRef = React.createRef();
+    this.bookreader = {};
   }
 
   componentDidMount() {
@@ -38,7 +39,6 @@ export default class BookReaderWrapper extends Component {
     const originalGetPageURI = BookReader.prototype.getPageURI;
     const defaultOptions = {
       el: `#${this.BookReaderRef.current.id}`,
-      mobileNavFullscreenOnly: true,
       onePage: { autofit: 'height' }, // options: auto, width, height
       ui: 'full',
       enablePageResume: false,
@@ -49,11 +49,19 @@ export default class BookReaderWrapper extends Component {
       searchInsideUrl: '/fulltext/inside.php',
       initialSearchTerm: null,
       imagesBaseURL: '/bookreader/BookReader/images/',
-      //TODO-ISA what is going on here, a comment would be useful ? (same on bookreader-wrapper-main and bookreader-wrapper-jsia)
+      useSrcSet: false,
+      /**
+       * Needed bypass to generate Image URL with scale factor.
+       * We must eliminate sooner than later to allow BookReader full image fetching control
+       * @param {Number} index - page index
+       * @param {Number} reduce - image size scale factor
+       * @param {Number} rotate - degrees of rotation
+       */
       getPageURI: (index, reduce = 1, rotate = 0) => {
-        let uri = originalGetPageURI.call(br, index, reduce, rotate);
+        const brReduce = this.bookreader.reduce || reduce;
+        let uri = originalGetPageURI.call(br, index, brReduce, rotate);
         uri += (uri.indexOf('?') > -1 ? '&' : '?');
-        uri = `${uri}scale=${reduce}&rotate=${rotate}`;
+        uri = `${uri}scale=${brReduce}&rotate=${rotate}`;
         return uri;
       },
       controls: {
@@ -73,6 +81,7 @@ export default class BookReaderWrapper extends Component {
     } else {
       const br = new BookReader(fullOptions);
       window.br = br;
+      this.bookreader = window.br;
       br.init();
     }
   }
