c685bd9ac6c397d7377984f7d825f243994a6f62
Isa Herico-Velasco <isaherico@gmail.com>
2021-03-01 09:01:04 -0800

    move components out of sandbox

--- packages/ia-components/index.js
+++ packages/ia-components/index.js
@@ -2,13 +2,9 @@
  * Export all of the public components here
  */
 
-// live
-export { default as IAUXExampleComponent } from './live/example-react-component/index';
+export { default as HorizontalRadioGroup } from './components/selectors/horizontal-radio-group/horizontal-radio-group';
+export { default as FlexboxPagination } from './components/pagination/pagination-with-flexbox';
 
-// sandbox
-export { default as HorizontalRadioGroup } from './sandbox/selectors/horizontal-radio-group/horizontal-radio-group';
-export { default as FlexboxPagination } from './sandbox/pagination/pagination-with-flexbox';
-
-export { default as TheatreTrackList } from './sandbox/track-lists/track-list';
-export { default as TheatreAudioPlayer } from './sandbox/audio-players/audio-player-main';
-export { default as AudioPlayerWithYoutubeSpotify } from './sandbox/theatres/with-youtube-spotify/audio-with-youtube-spotify-main';
+export { default as TheatreTrackList } from './components/track-lists/track-list';
+export { default as TheatreAudioPlayer } from './components/audio-players/audio-player-main';
+export { default as AudioPlayerWithYoutubeSpotify } from './components/theatres/with-youtube-spotify/audio-with-youtube-spotify-main';
