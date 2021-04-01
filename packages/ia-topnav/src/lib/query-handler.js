05b995721af14f4a2aa78da3903008b03bc35aba
Neeraj Sharma <50868725+nsharma123@users.noreply.github.com>
2021-04-01 21:26:16 +0530

    WEBDEV-4189 Removed extra URL encoding for wayback search component (#542)
    * Removed extra URL encoding for wayback search component
    
    * New version for ia-topnav

--- packages/ia-topnav/src/lib/query-handler.js
+++ packages/ia-topnav/src/lib/query-handler.js
@@ -2,6 +2,6 @@
 
 export default {
   performQuery(query) {
-    window.location = `https://web.archive.org/web/*/${encodeURIComponent(query)}`;
+    window.location = `https://web.archive.org/web/*/${query}`;
   }
 };
