eb8aca0e2fee89b087293ef1bfecd8354f3744be
Jason Buckner <jason@jasonbuckner.com>
2021-04-06 10:16:37 -0700

    Add types

---
+++ packages/ia-topnav/index.d.ts
@@ -0,0 +1,9 @@
+import { LitElement } from 'lit-element';
+
+export declare class IATopNav extends LitElement {
+  baseHost: string;
+  username?: string;
+  screenName?: string;
+  searchQuery?: string;
+  hideSearch: boolean;
+}
