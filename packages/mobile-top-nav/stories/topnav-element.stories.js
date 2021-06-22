7d61bf38b7067f2d191d03487a05b74536e5eb29
Jason Buckner <jason@jasonbuckner.com>
2021-06-22 13:51:55 -0700

    Remove mobile top-nav
    This has been rolled into ia-topnav

--- packages/mobile-top-nav/stories/topnav-element.stories.js
+++
@@ -1,7 +0,0 @@
-import { storiesOf, withKnobs, withClassPropertiesKnobs } from '@open-wc/demoing-storybook';
-
-import IAMobileTopNav from '../index';
-
-storiesOf('IAMobileTopNav', module)
-  .addDecorator(withKnobs)
-  .add('Mobile top nav options', () => withClassPropertiesKnobs(IAMobileTopNav));
