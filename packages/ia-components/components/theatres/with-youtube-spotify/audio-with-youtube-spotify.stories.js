c77b7beb2820e082e9943f8beedca96f1b4a2307
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2022-01-05 10:54:29 -0800

    liner notes player: use image in item in media window instead of image placeholder (#616)
    * update liner notes storybook demo
    * liner notes player view model: catch image to display
    * add new item stub to display 78s with only 1 image in file stack
    * update regex to look at EOL

--- packages/ia-components/components/theatres/with-youtube-spotify/audio-with-youtube-spotify.stories.js
+++ packages/ia-components/components/theatres/with-youtube-spotify/audio-with-youtube-spotify.stories.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import albumData from '../../data-for-stories/album-78-w-one-image';
 
-import albumData from '../../data-for-stories/album-private-with-spotify-youtube';
+// import albumData from '../../data-for-stories/album-private-with-spotify-youtube';
 import AudioPlayerWithYoutubeSpotify from './audio-with-youtube-spotify-main';
 import './audio-with-youtube-spotify.less';
 
 const { itemInfo, jwplayerPlaylist, linerNotes } = albumData;
+
+const items = [
+  {
+    id: 'capitol-15045-b-cigarettes-whiskey-and-wild-wild-women',
+    desc: '78 - w/o jp2 (only 1 item image)'
+  },
+  {
+    id: 'bestofdollyparto00part',
+    desc: 'LP - older'
+  },
+  {
+    id: 'lp_dancing-tonight_freddy-martin-and-his-orchestra',
+    desc: 'LP - current, ~ 2020'
+  },
+  {
+    id: 'cd_beethoven-complete-works-for-string-trio_the-adaskin-string-trio',
+    desc: 'what_cd'
+  },
+  {
+    id: 'wcd_message-in-a-box-th_the-police_flac_lossless_807968',
+    desc: 'Irregular Photo - (portrait)'
+  },
+  {
+    id: 'lak-JC_Burris-James_Booker',
+    desc: 'No photo + long track list'
+  },
+  {
+    id: 'wcd_various-artiststhe-best-of-country-music_flac_lossless_29887623',
+    desc: 'Complilation, various artists'
+  },
+  {
+    id: 'lp_emperor-concerto_ludwig-van-beethoven-arthur-rubinstein-bos',
+    desc: 'Track names, multiple but same as album artist (should be omitted)'
+  },
+  {
+    id: 'illegal-art',
+    desc: '3 column track list wide view pagination check'
+  },
+  {
+    id: 'wcd_borghild_die-warzau_mp3_320_1648819',
+    desc: 'Track time display, 60 seconds adds another minute. should display as 10:00'
+  },
+  {
+    id: 'cd_aaliyah_aaliyah-static-from-playa-timbaland',
+    desc: 'Has 3rd party "Full Album". Clicking on Full Album should highlight full album'
+  },
+];
 
 /**
  * Component wrapper around the music player
@@ -85,7 +133,7 @@ class DataHydrator extends Component {
     const { itemInfo, jwplayerPlaylist, error } = this.state;
     const { metadata: { identifier } } = itemInfo;
 
-    console.log('***** RENDER: ', itemInfo, jwplayerPlaylist.length);
+    console.log('***** RENDER: ', { itemInfo, error, jwplayerPlaylist });
     // using bootstrap v3 styling for container for pretty UI
     return (
       <div>
@@ -113,144 +161,47 @@ class DataHydrator extends Component {
         <section className="container">
           <h3>Supported Items</h3>
           <h4>Archive </h4>
-          <table>
-            <thead>
-              <tr>Collection</tr>
-              <tr>Identifier</tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>album_recordings (Older LP)</td>
-                <td>
-                  <button
-                    type="button"
-                    className="btn btn-lg btn-warning"
-                    data-identifier="bestofdollyparto00part"
-                    onClick={this.getItem}
-                  >
-                    bestofdollyparto00part
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>album_recordings (Latest LP)</td>
-                <td>
-                  <button
-                    type="button"
-                    className="btn btn-lg btn-info"
-                    data-identifier="lp_dancing-tonight_freddy-martin-and-his-orchestra"
-                    onClick={this.getItem}
-                  >
-                    lp_dancing-tonight_freddy-martin-and-his-orchestra
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>what_cd</td>
-                <td>
-                  <button
-                    type="button"
-                    className="btn btn-lg btn-success"
-                    data-identifier="cd_beethoven-complete-works-for-string-trio_the-adaskin-string-trio"
-                    onClick={this.getItem}
-                  >
-                    cd_beethoven-complete-works-for-string-trio_the-adaskin-string-trio
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>what_cd (Irregular Photo)</td>
-                <td>
-                  <button
-                    type="button"
-                    className="btn btn-lg btn-success"
-                    data-identifier="wcd_message-in-a-box-th_the-police_flac_lossless_807968"
-                    onClick={this.getItem}
-                  >
-                    wcd_message-in-a-box-th_the-police_flac_lossless_807968
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>acdc (NO PHOTO)</td>
-                <td>
-                  <button
-                    type="button"
-                    className="btn btn-lg btn-primary"
-                    data-identifier="lak-JC_Burris-James_Booker"
-                    onClick={this.getItem}
-                  >
-                    lak-JC_Burris-James_Booker
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>what CD compilation - all track artists names show</td>
-                <td>
-                  <button
-                    type="button"
-                    className="btn btn-lg btn-primary"
-                    data-identifier="wcd_various-artiststhe-best-of-country-music_flac_lossless_29887623"
-                    onClick={this.getItem}
-                  >
-                  wcd_various-artiststhe-best-of-country-music_flac_lossless_29887623
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>Track names, multiple but same as album artist (should be omitted)</td>
-                <td>
-                  <button
-                    type="button"
-                    className="btn btn-lg btn-primary"
-                    data-identifier="lp_emperor-concerto_ludwig-van-beethoven-arthur-rubinstein-bos"
-                    onClick={this.getItem}
-                  >
-                  lp_emperor-concerto_ludwig-van-beethoven-arthur-rubinstein-bos
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>3 column track list wide view pagination check</td>
-                <td>
-                  <button
-                    type="button"
-                    className="btn btn-lg btn-primary"
-                    data-identifier="illegal-art"
-                    onClick={this.getItem}
-                  >
-                  illegal-art
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>Track time display, 60 seconds adds another minute. should display as 10:00</td>
-                <td>
-                  <button
-                    type="button"
-                    className="btn btn-lg btn-primary"
-                    data-identifier="wcd_borghild_die-warzau_mp3_320_1648819"
-                    onClick={this.getItem}
-                  >
-                  wcd_borghild_die-warzau_mp3_320_1648819
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td>Has 3rd party "Full Album". Clicking on Full Album should highlight full album</td>
-                <td>
-                  <button
-                    type="button"
-                    className="btn btn-lg btn-primary"
-                    data-identifier="cd_aaliyah_aaliyah-static-from-playa-timbaland"
-                    onClick={this.getItem}
-                  >
-                  cd_aaliyah_aaliyah-static-from-playa-timbaland
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <div id="liner-notes-list">
+            <table id="examples">
+              <thead>
+                <tr>
+                  <th scope="col">item actions</th>
+                  <th scope="col">ID</th>
+                  <th scope="col">it's quirks</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                items.map((item) => {
+                  const { id, desc } = item;
+                  return (
+                    <tr>
+                      <td>
+                        <button
+                          type="button"
+                          className="btn btn-info"
+                          data-identifier={id}
+                          onClick={this.getItem}
+                        >
+                          Show
+                        </button>
+                        <a className="btn btn-link" target="_blank" href="https://archive.org/metadata/${id}">Metadata</a>
+                        <a className="btn btn-link" target="_blank" href="https://archive.org/details/${id}">Details</a>
+                      </td>
+                      <td>
+                        {id}
+                      </td>
+                      <td>
+                        {desc}
+                      </td>
+                    </tr>
+                  );
+                })
+              }
+              </tbody>
+            </table>
+          </div>
+
         </section>
       </div>
     );
