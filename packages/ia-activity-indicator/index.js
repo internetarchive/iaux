ce71c2d6d20273787f45dc4e4d529dcb4ce552b3
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2023-05-22 21:44:49 -0700

    Define activity indicator in main indexjs (#859)
    * ia-activity-indicator - define at index.js for esm
    
    * make demo load from main index.js
    
    * package-lock.json

--- packages/ia-activity-indicator/index.js
+++ packages/ia-activity-indicator/index.js
@@ -1,1 +1,5 @@
+import { IAActivityIndicator } from './src/ia-activity-indicator.js';
+
+window.customElements.define('ia-activity-indicator', IAActivityIndicator);
+
 export { IAActivityIndicator, IAActivityIndicatorMode } from './src/ia-activity-indicator.js';
