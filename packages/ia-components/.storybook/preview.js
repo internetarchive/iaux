fe121666d27c4c4690c44d20f7486df9b0cdc065
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2023-06-08 13:53:04 -0700

    remove ia-components

--- packages/ia-components/.storybook/preview.js
+++
@@ -1,6 +0,0 @@
-import '!style-loader!css-loader!less-loader!../index.less';
-import '!style-loader!css-loader!less-loader!./theatre-styling.less';
-
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
--- packages/ia-components/bin/copy_bookreader.js
+++
@@ -1,4 +0,0 @@
-const { execSync } = require('child_process');
-
-execSync('mkdir -p public');
-execSync('cp -r node_modules/@internetarchive/bookreader/BookReader public/');
