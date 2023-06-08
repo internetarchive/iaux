bbc151f7665f9f44e0d09f713ad110df9e1a9631
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2023-06-08 13:55:26 -0700

    Deprecate ia-js-client

--- packages/ia-js-client/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { MetadataService } from './services/metadata'
-export { RelatedService } from './services/related'
-export { BookReaderJsIaService } from './services/bookreader-jsia'
-export { Item } from './controllers/item/item'
-export { BookController } from './controllers/book/book'
