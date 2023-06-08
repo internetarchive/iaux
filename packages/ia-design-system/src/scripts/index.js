0189efce620fb6d8484d5e494e09d259e9960370
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2023-06-08 13:54:13 -0700

    Deprecate ia-design-system

--- packages/ia-design-system/src/scripts/index.js
+++
@@ -1,7 +0,0 @@
-const IaDesignSystem = {};
-
-// Add components as keys in the IaDesignSystem object, for example:
-//    import MyComponent from 'components/MyComponent';
-//    IaDesignSystem.MyComponent = MyComponent;
-
-export default IaDesignSystem;
