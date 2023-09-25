2432626c28de9263fdb296e609cebcd732bfb765
Neeraj Sharma <50868725+nsharma123@users.noreply.github.com>
2023-09-25 13:18:44 +0530

    Userlist settings change datamodel (#871)
    * Updated css tweaks on iaux-userlist-settings

--- packages/iaux-userlist-settings/src/iaux-userlist-settings.ts
+++ packages/iaux-userlist-settings/src/iaux-userlist-settings.ts
@@ -1,23 +1,25 @@
 /* eslint-disable no-undef */
 /* eslint-disable lit/no-value-attribute */
 
-import { html, css, LitElement } from 'lit';
+import { html, css, LitElement, TemplateResult } from 'lit';
 import { property, customElement, query } from 'lit-element/decorators.js';
 import IAButtonStyles from './style/ia-button';
 
-export type UserListInfo = {
-  id: string;
-  name: string;
-  description: string;
-  private: boolean;
-};
+export interface UserListModel {
+  id?: string;
+  list_name: string | TemplateResult;
+  description?: string | TemplateResult;
+  is_private?: boolean;
+  date_created?: Date;
+  date_updated?: Date;
+}
 
 @customElement('iaux-userlist-settings')
 export class IAUXUserListSettings extends LitElement {
   /**
    * contains userlist information
    */
-  @property({ type: Object }) listInfo: UserListInfo;
+  @property({ type: Object }) userList?: UserListModel;
 
   /**
    * base api URL for userlist service
@@ -50,7 +52,7 @@ export class IAUXUserListSettings extends LitElement {
       console.log('response', response);
 
       this.dispatchEvent(
-        new CustomEvent('listDetailsSaved', {
+        new CustomEvent('userListSaved', {
           detail: {
             inputData: requestInit,
             outputData: response,
@@ -58,6 +60,11 @@ export class IAUXUserListSettings extends LitElement {
         })
       );
     } catch (error) {
+      this.dispatchEvent(
+        new CustomEvent('userListSavedError', {
+          detail: { error },
+        })
+      );
       console.log('error', error);
     }
   }
@@ -71,12 +78,12 @@ export class IAUXUserListSettings extends LitElement {
       <section class="new-list">
         <form id="user-list-form" @submit=${this.saveListDetails}>
           <div class="field">
-            <input type="hidden" id="id" .value=${this.listInfo?.id} />
+            <input type="hidden" id="id" .value=${this.userList?.id} />
             <label for="name">List name*</label>
             <input
               type="text"
               id="name"
-              value=${this.listInfo?.name}
+              value=${this.userList?.list_name}
               required
             />
           </div>
@@ -84,7 +91,7 @@ export class IAUXUserListSettings extends LitElement {
             <label for="description">Description</label>
             <textarea
               id="description"
-              .value=${this.listInfo?.description ?? ''}
+              .value=${this.userList?.description ?? ''}
             ></textarea>
           </div>
           <div class="field">
@@ -92,7 +99,7 @@ export class IAUXUserListSettings extends LitElement {
             <input
               type="checkbox"
               id="private"
-              .checked="${this.listInfo?.private}"
+              .checked="${this.userList?.is_private}"
             />
           </div>
           <div class="footer field">
@@ -128,9 +135,10 @@ export class IAUXUserListSettings extends LitElement {
 
       label {
         display: inline-block;
-        width: 85px;
+        width: 90px;
         text-align: left;
         margin-right: 10px;
+        font-weight: 700;
       }
 
       input[type='text'],
