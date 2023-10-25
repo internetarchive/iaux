9b7f9102e2581981fc275bb506a0aaa30f445aee
Mark Carranza <mark.carranza@gmail.com>
2023-10-25 15:08:16 -0700

    Comments

--- packages/ia-topnav/src/primary-nav.js
+++ packages/ia-topnav/src/primary-nav.js
@@ -80,6 +80,7 @@ class PrimaryNav extends TrackedElement {
   }
 
   get isRTL() {
+    // https://stackoverflow.com/questions/12006095/javascript-how-to-check-if-character-is-rtl
     const ltrChars = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF'+'\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF';
     const rtlChars = '\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC';
     const rtlCheck = new RegExp('^[^'+ltrChars+']*['+rtlChars+']');
