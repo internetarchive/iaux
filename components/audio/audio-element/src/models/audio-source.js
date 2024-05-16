298852294230f5a41eed567d61e2fdd58713b581
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2024-05-16 10:29:42 -0700

    remove build files

--- components/audio/audio-element/src/models/audio-source.js
+++
@@ -1,9 +0,0 @@
-export default class AudioSource {
-    url;
-    mimetype;
-    constructor(url, mimetype) {
-        this.url = url;
-        this.mimetype = mimetype;
-    }
-}
-//# sourceMappingURL=audio-source.js.map
