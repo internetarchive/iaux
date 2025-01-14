d2445a7f6890f9d4cf92551e7d0cc1d30611efd7
Jason Buckner <jason@jasonbuckner.com>
2025-01-14 15:47:04 -0800

    Fix ia-activity-indicator double-registration

--- packages/ia-activity-indicator/index.js
+++ packages/ia-activity-indicator/index.js
@@ -1,5 +1,5 @@
-import { IAActivityIndicator } from './src/ia-activity-indicator.js';
+import { IAActivityIndicator, IAActivityIndicatorMode } from './src/ia-activity-indicator.js';
 
 window.customElements.define('ia-activity-indicator', IAActivityIndicator);
 
-export { IAActivityIndicator, IAActivityIndicatorMode } from './src/ia-activity-indicator.js';
+export { IAActivityIndicator, IAActivityIndicatorMode };
