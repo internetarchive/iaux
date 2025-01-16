54427a8f26daffe190a17f89f4cd033b49522b81
Jason Buckner <jason@jasonbuckner.com>
2025-01-16 15:25:43 -0800

    Add compat test

---
+++ packages/ia-activity-indicator/test/backwards-compat.test.js
@@ -0,0 +1,13 @@
+import { html, fixture, expect } from '@open-wc/testing';
+
+import '../ia-activity-indicator.js';
+
+describe('IA Activity Indicator backwards compatibility import', () => {
+  it('can import the top-level ia-activity-indicator.js file and get an indicator', async () => {
+    const el = await fixture(html`
+      <ia-activity-indicator></ia-activity-indicator>
+    `);
+    const container = el.shadowRoot.querySelectorAll('div');
+    expect(container.length).to.equal(1);
+  });
+});
