a651aa1d1870bc8b2ba033eddf072c8ab02b2da9
Jason Buckner <jason@jasonbuckner.com>
2025-03-11 19:11:39 -0700

    Move styling to app-root

--- packages/ia-pic-uploader/demo/app-root.ts
+++ packages/ia-pic-uploader/demo/app-root.ts
@@ -1,4 +1,4 @@
-import { html, LitElement } from 'lit';
+import { css, CSSResultGroup, html, LitElement } from 'lit';
 import { customElement, query } from 'lit/decorators.js';
 
 import '../src/ia-pic-uploader.js';
@@ -12,38 +12,86 @@ export class AppRoot extends LitElement {
 
   render() {
     return html`
-      <div class="container">
-        <div class="full">
-          <h3>It will be used at My Uploads page</h3>
-          <ia-pic-uploader
-            lookingAtMyAccount
-            maxFileSizeInMB="3"
-            id="full"
-            identifier="@453344354534"
-            picture="/demo/wider-image.jpg"
-            endpoint=""
-            .validFileTypes=${['image/jpeg', 'image/png', 'image/gif']}
-            type="full"
-          ></ia-pic-uploader>
-        </div>
-        <div class="compact">
-          <h3>It will be used at new account settings page</h3>
-          <ia-pic-uploader
-            lookingAtMyAccount
-            maxFileSizeInMB="3"
-            id="compact"
-            identifier="@453344354534"
-            picture="/demo/default-preview.jpg"
-            @fileChanged=${(e: CustomEvent) => {
-              console.log('called hasErrorFiled()!');
-              console.log(e.detail);
-            }}
-            @fileUploaded=${() => {
-              console.log('file Uploaded');
-            }}
-          ></ia-pic-uploader>
+      <div id="demo"></div>
+        <div class="container">
+          <div class="full">
+            <h3>It will be used at My Uploads page</h3>
+            <ia-pic-uploader
+              lookingAtMyAccount
+              maxFileSizeInMB="3"
+              id="full"
+              identifier="@453344354534"
+              picture="/demo/wider-image.jpg"
+              endpoint=""
+              .validFileTypes=${['image/jpeg', 'image/png', 'image/gif']}
+              type="full"
+            ></ia-pic-uploader>
+          </div>
+          <div class="compact">
+            <h3>It will be used at new account settings page</h3>
+            <ia-pic-uploader
+              lookingAtMyAccount
+              maxFileSizeInMB="3"
+              id="compact"
+              identifier="@453344354534"
+              picture="/demo/default-preview.jpg"
+              @fileChanged=${(e: CustomEvent) => {
+        console.log('called hasErrorFiled()!');
+        console.log(e.detail);
+      }}
+              @fileUploaded=${() => {
+        console.log('file Uploaded');
+      }}
+            ></ia-pic-uploader>
+          </div>
         </div>
       </div>
     `;
+  }
+
+  static get styles(): CSSResultGroup {
+    return css`
+      #demo {
+        margin: 50px;
+        display: flex;
+        justify-items: center;
+        align-items: center;
+        font-size: 14px;
+      }
+
+      .select-message {
+        display: none;
+      }
+
+      ia-pic-uploader {
+        --imgMaxHeight: 200px;
+        --imgMaxWidth: 200px;
+      }
+      ia-pic-uploader:focus {
+        outline: none;
+      }
+
+      ia-pic-uploader:focus-visible {
+        outline: none;
+      }
+
+      .container {
+        display: flex;
+        width: 100%;
+        justify-content: center;
+        flex-direction: column;
+      }
+
+      .full {
+        display: flex;
+        flex-direction: column;
+      }
+
+      .full, .compact {
+        padding: 25px 50px;
+        border: 1px solid black;
+        margin: 20px;
+      }
+    `
   }
 }
