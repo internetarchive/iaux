02ba9243a00148fb2a1e91f817f34202ce3c2fd7
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2025-04-21 20:09:53 -0700

    use npm instead of yarn

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "$SCRIPT_DIR/../common" && yarn install
-for dir in $SCRIPT_DIR/../packages/*; do (cd "$dir" && yarn install); done
+cd "$SCRIPT_DIR/../common" && npm install
+for dir in $SCRIPT_DIR/../packages/*; do (cd "$dir" && npm install); done
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-for dir in $SCRIPT_DIR/../packages/*; do (cd "$dir" && yarn run test); done
+for dir in $SCRIPT_DIR/../packages/*; do (cd "$dir" && npm run test); done
