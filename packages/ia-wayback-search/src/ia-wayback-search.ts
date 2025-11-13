fdf35f4fa829de7e05bd601a83f9fc626ef7c867
Neeraj Sharma <50868725+nsharma123@users.noreply.github.com>
2025-11-14 00:06:00 +0530

    Wayback: Add label accessible to wayback search form (#979)

--- packages/ia-wayback-search/src/ia-wayback-search.ts
+++ packages/ia-wayback-search/src/ia-wayback-search.ts
@@ -1,5 +1,6 @@
 import { css, CSSResultGroup, html, LitElement } from 'lit';
 import { customElement, property, query } from 'lit/decorators.js';
+import { iaSronlyStyles } from '@internetarchive/ia-styles';
 import searchIcon from './icon-search';
 import logo from './logo';
 
@@ -37,8 +38,8 @@ export class WaybackSearch extends LitElement {
             href="https://web.archive.org"
             >${logo}</a
           >
-          <label for="url">Search the Wayback Machine</label>
           <div class="search-field">
+            <label for="url" class="sr-only">Search the Wayback Machine</label>
             <input
               type="text"
               name="url"
@@ -77,125 +78,124 @@ export class WaybackSearch extends LitElement {
     this.dispatchEvent(new CustomEvent('waybackMachineLogoLink'));
   }
 
-  static styles: CSSResultGroup = css`
-    :host {
-      font: normal 1.2rem/1.5 var(--themeFontFamily);
-    }
+  static styles: CSSResultGroup = [
+    iaSronlyStyles,
+    css`
+      :host {
+        font: normal 1.2rem/1.5 var(--themeFontFamily);
+      }
 
-    form {
-      max-width: 600px;
-    }
-
-    p {
-      margin-top: 0;
-      font-weight: 200;
-    }
-
-    a {
-      font-weight: 500;
-      text-decoration: none;
-      color: var(--activeColor);
-    }
-
-    fieldset {
-      padding: 0.7rem 2rem;
-      margin: 1.5rem 0;
-      box-sizing: border-box;
-      text-align: center;
-      border: none;
-      border-radius: 7px;
-      background-color: #fcf5e6;
-      box-shadow: 3px 3px 0 0 #c3ad97;
-    }
-
-    fieldset a {
-      font-size: 0;
-    }
-
-    label {
-      display: none;
-    }
-
-    img {
-      width: 100%;
-      max-width: 215px;
-      max-height: 60px;
-      margin-bottom: 1.3rem;
-      vertical-align: middle;
-    }
-
-    input {
-      display: block;
-      width: 100%;
-      height: 3rem;
-      padding: 0.5rem 1rem 0.5rem 3rem;
-      font: normal 1.2rem/1.5 var(--themeFontFamily);
-      color: var(--ia-theme-secondary-text-color, #666);
-      box-sizing: border-box;
-      border: 1px solid var(--grey80);
-      border-radius: 2rem;
-      background: var(--ia-theme-secondary-background-color, #fff);
-    }
-
-    input:focus {
-      border-color: #66afe9;
-      outline: none;
-    }
-
-    .search-field {
-      position: relative;
-      overflow: hidden;
-    }
-
-    .search-field svg {
-      position: absolute;
-      top: 3px;
-      left: 3px;
-      width: 2.4rem;
-      height: 2.4rem;
-    }
-
-    .search-field .fill-color {
-      fill: var(--iconFill);
-    }
-
-    @media (min-width: 890px) {
       form {
-        margin: 0 auto;
+        max-width: 600px;
       }
 
       p {
-        margin-bottom: 3rem;
-        font-size: 1.6rem;
-        text-align: center;
+        margin-top: 0;
+        font-weight: 200;
       }
 
-      img {
-        margin: 0;
+      a {
+        font-weight: 500;
+        text-decoration: none;
+        color: var(--activeColor);
       }
 
       fieldset {
-        margin: 0 auto;
-      }
-
-      fieldset a,
-      .search-field {
-        display: inline-block;
-        width: 49%;
-        vertical-align: middle;
+        padding: 0.7rem 2rem;
+        margin: 1.5rem 0;
+        box-sizing: border-box;
+        text-align: center;
+        border: none;
+        border-radius: 7px;
+        background-color: #fcf5e6;
+        box-shadow: 3px 3px 0 0 #c3ad97;
       }
 
       fieldset a {
-        text-align: center;
+        font-size: 0;
+      }
+
+      img {
+        width: 100%;
+        max-width: 215px;
+        max-height: 60px;
+        margin-bottom: 1.3rem;
+        vertical-align: middle;
+      }
+
+      input {
+        display: block;
+        width: 100%;
+        height: 3rem;
+        padding: 0.5rem 1rem 0.5rem 3rem;
+        font: normal 1.2rem/1.5 var(--themeFontFamily);
+        color: var(--ia-theme-secondary-text-color, #666);
+        box-sizing: border-box;
+        border: 1px solid var(--grey80);
+        border-radius: 2rem;
+        background: var(--ia-theme-secondary-background-color, #fff);
+      }
+
+      input:focus {
+        border-color: #66afe9;
+        outline: none;
+      }
+
+      .search-field {
+        position: relative;
+        overflow: hidden;
       }
 
       .search-field svg {
-        top: 2px;
+        position: absolute;
+        top: 3px;
+        left: 3px;
+        width: 2.4rem;
+        height: 2.4rem;
       }
 
       .search-field .fill-color {
-        fill: var(--desktopSearchIconFill);
+        fill: var(--iconFill);
       }
-    }
-  `;
+
+      @media (min-width: 890px) {
+        form {
+          margin: 0 auto;
+        }
+
+        p {
+          margin-bottom: 3rem;
+          font-size: 1.6rem;
+          text-align: center;
+        }
+
+        img {
+          margin: 0;
+        }
+
+        fieldset {
+          margin: 0 auto;
+        }
+
+        fieldset a,
+        .search-field {
+          display: inline-block;
+          width: 49%;
+          vertical-align: middle;
+        }
+
+        fieldset a {
+          text-align: center;
+        }
+
+        .search-field svg {
+          top: 2px;
+        }
+
+        .search-field .fill-color {
+          fill: var(--desktopSearchIconFill);
+        }
+      }
+    `,
+  ];
 }
